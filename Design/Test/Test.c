88ccf21672a17c7045348241b2657cd94857e8dd
sabertazimi <sabertazimi@gmail.com>
2016-01-28 11:49:24 +0800

    fix(Test):fix Travis CI bug Mistakely removing *.o lead to there is no executable avilable.To fix that, compile .c to .exe.

--- Design/Test/Test.c
+++ Design/Test/Test.c
@@ -1,13 +1,13 @@
 /*************************************************************************
 	> File Name: Test.c
-	> Author: 
-	> Mail: 
+	> Author: sabertazimi
+	> Mail: sabertazimi@gmail.com
 	> Created Time: 2016年01月28日 星期四 11时17分22秒
  ************************************************************************/
 
 #include<stdio.h>
 
 int main(void){
-    printf("Build Travis CI");
+    printf("Build Travis CI\n");
     return 0;
 }
