762e5472ae80e8196a1c2b170f1837a9f3139233
sabertazimi <sabertazimi@gmail.com>
2016-03-20 19:40:29 +0800

    Update(Lab Two): system v1.0
    TODO: fix bugs

--- lab2/src/grade.asm
+++ lab2/src/grade.asm
@@ -4,26 +4,46 @@ STACK   SEGMENT     USE16   STACK
         DB 200  DUP(0)
 STACK   ENDS
 
+
 DATA    SEGMENT     USE16
-        NUM     EQU 1
+        NUM     EQU 3
         BUF     DB  'saber'
-        LEN     EQU $-BUF
-        ZLEN    EQU 10-LEN
-                DB  ZLEN DUP(0)
+        LEN1    EQU $-BUF
+        ZLEN1   EQU 10-LEN1
+                DB  ZLEN1 DUP(0)
         ZH      DB  90
         MA      DB  80
         EN      DB  85
         AVG     DB  ?
-        MSG     DB  'Please input target name: $'
-        FAIL    DB  'Not Exist\n$'
+        ARCH    DB  'archer'
+        LEN2    EQU $-ARCH
+        ZLEN2   EQU 10-LEN2
+                DB  ZLEN2 DUP(0)
+                DB  80
+                DB  75
+                DB  85
+                DB  ?
+        LANC    DB  'lancer'
+        LEN3    EQU $-LANC
+        ZLEN3   EQU 10-LEN3
+                DB  ZLEN3 DUP(0)
+                DB  70
+                DB  80
+                DB  75
+                DB  ?
+        MSG     DB  'Please input target name (end with $): $'
+        CONTINU DB  'Enter any keys to continue(except q):$'
+        FAIL    DB  'Not Exist!$'
         INPUT   DB  10 DUP(10)
 DATA    ENDS
 
+
 CODE    SEGMENT     USE16
         ASSUME  CS:CODE, DS:DATA, SS:STACK
+
 START:  MOV     AX, DATA
         MOV     DS, AX
-        ;JMP     READ
+        JMP     READ
 
 GRADEA: MOV     DL, 41H
         MOV     AH, 2H
@@ -46,21 +66,52 @@ GRADEE: MOV     DL, 45H
         INT     21H
         JMP     READY
 
-READY:  MOV     AH, 4CH
+READY:  LEA     DX, OFFSET CONTINU  ; 输出提示信息
+        MOV     AH, 9H
         INT     21H
+        MOV     DL, 0AH     ; 输出换行符
+        MOV     AH, 2H
+        INT     21H
+        MOV     AH, 8H
+        INT     21H
+        CMP     AL, 71H
+        JE      OVER        ; 输入 'q', 退出程序
 
 READ:   MOV     CX, NUM     ; 学生个数
-        LEA     DX, OFFSET MSG
+        LEA     DX, OFFSET MSG     ; 输出提示信息
         MOV     AH, 9H
+        INT     21H
+        MOV     DL, 0AH     ; 输出换行符
+        MOV     AH, 2H
         INT     21H
         LEA     DX, OFFSET INPUT   ; 输入学生姓名, 以 '$' 符号结尾
         MOV     AH, 0AH
         INT     21H
-        ; LEA     DX, INPUT
-        ; MOV     AH, 9H
-        ; INT     21H
-        MOV     AH, 4CH
+
+        MOV     DL, 0AH     ; 输出换行符
+        MOV     AH, 2H
         INT     21H
+        LEA     DX, OFFSET INPUT
+        MOV     AH, 9H
+        INT     21H
+
+        LEA     BP, OFFSET INPUT; 将 INPUT 基址存放至 BP
+        INC     BP
+        INC     CX
+COMPA:  DEC     CX
+        JE      READY       ; 查找失败, 重新输入
+        MOV     BX, NUM     ; 计算目标学生下标值, 存放至 BX
+        SUB     BX, CX
+        IMUL    BX, 14      ; 根据目标学生下标值, 找到分数缓冲区首地址
+COMPB:  MOV     AX, 10      ; 临时计数器
+        MOV     SI, 0
+        MOV     DL, [BX + SI]
+        MOV     DH, BYTE PTR DS: [BP + SI]
+        CMP     DH, DL      ; 比较 当前缓冲区姓名 与 输入姓名 字符
+        JNE     COMPA
+        INC     SI
+        DEC     AX
+        JNE     COMPB
 
 CAL:    MOV     BX, NUM     ; 计算目标学生下标值, 存放至 BX
         SUB     BX, CX
@@ -72,10 +123,11 @@ CAL:    MOV     BX, NUM     ; 计算目标学生下标值, 存放至 BX
         MOV     DL, [BX + 2]
         SAR     DL, 1
         ADD     AL, DL      ; AL = ZH * 2 + MA + EN / 2
-        MOV     DL, AL
-        SAL     AL, 3
-        SUB     AL, DL      ; 7AL = 8AL - AL
-        SAR     AL, 2       ; 3.5AL = 7AL / 2
+        SAL     AL, 2       ; AL = 2 * AL
+        MOV     AH, 0
+        MOV     DX, 7
+        IDIV    DX          ; AL = AL / 7
+        MOV     [BX + 3], AL; AVG = AL ( AL / 3.5)
         CMP     AX, 90      ; switch 语句
         JBE     GRADEA
         CMP     AX, 80
@@ -85,6 +137,9 @@ CAL:    MOV     BX, NUM     ; 计算目标学生下标值, 存放至 BX
         CMP     AX, 60
         JBE     GRADED
         JMP     GRADEE
+
+OVER:   MOV     AH, 4CH
+        INT     21H
 
 CODE    ENDS
         END     START
