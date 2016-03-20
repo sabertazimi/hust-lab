a2f5899bd062bf78ac46ef3ceb9eab508695d5e7
sabertazimi <sabertazimi@gmail.com>
2016-03-20 21:06:00 +0800

    Update(Lab Two): complete lab two yahooo
    fucking bugs

--- lab2/src/grade.asm
+++ lab2/src/grade.asm
@@ -66,7 +66,14 @@ GRADEE: MOV     DL, 45H
         INT     21H
         JMP     READY
 
-READY:  LEA     DX, OFFSET CONTINU  ; 输出提示信息
+
+FAILED: LEA     DX, OFFSET FAIL     ; 输出提示信息 - 查找失败
+        MOV     AH, 9H
+        INT     21H
+READY:  MOV     DL, 0AH     ; 输出换行符
+        MOV     AH, 2H
+        INT     21H
+        LEA     DX, OFFSET CONTINU  ; 输出提示信息 - 是否继续
         MOV     AH, 9H
         INT     21H
         MOV     DL, 0AH     ; 输出换行符
@@ -78,13 +85,13 @@ READY:  LEA     DX, OFFSET CONTINU  ; 输出提示信息
         JE      OVER        ; 输入 'q', 退出程序
 
 READ:   MOV     CX, NUM     ; 学生个数
-        LEA     DX, OFFSET MSG     ; 输出提示信息
+        LEA     DX, OFFSET MSG     ; 输出提示信息 - 请输入姓名
         MOV     AH, 9H
         INT     21H
         MOV     DL, 0AH     ; 输出换行符
         MOV     AH, 2H
         INT     21H
-        LEA     DX, OFFSET INPUT   ; 输入学生姓名, 以 '$' 符号结尾
+        LEA     DX, OFFSET INPUT   ; 读入学生姓名, 以 '$' 符号结尾
         MOV     AH, 0AH
         INT     21H
 
@@ -96,19 +103,21 @@ READ:   MOV     CX, NUM     ; 学生个数
         INT     21H
 
         LEA     BP, OFFSET INPUT; 将 INPUT 基址存放至 BP
-        INC     BP
+        ADD     BP,2        ; FIX ME:如果不+2修正,则取不到输入字符串,一定是我哪里写错了
         INC     CX
 COMPA:  DEC     CX
-        JE      READY       ; 查找失败, 重新输入
+        JE      FAILED      ; 查找失败, 重新输入
         MOV     BX, NUM     ; 计算目标学生下标值, 存放至 BX
         SUB     BX, CX
         IMUL    BX, 14      ; 根据目标学生下标值, 找到分数缓冲区首地址
-COMPB:  MOV     AX, 10      ; 临时计数器
+        MOV     AX, 10      ; 临时计数器
         MOV     SI, 0
-        MOV     DL, [BX + SI]
-        MOV     DH, BYTE PTR DS: [BP + SI]
+COMPB:  MOV     DL, [BX + SI]
+        MOV     DH, BYTE PTR DS:[BP + SI]
+        CMP     DL, 0       ; 如果缓冲区姓名已结束,说明查找成功
+        JE      CAL         ; 跳转至平均成绩计算处
         CMP     DH, DL      ; 比较 当前缓冲区姓名 与 输入姓名 字符
-        JNE     COMPA
+        JNE     COMPA       ; 当前字符相同,继续循环以比较下一字符
         INC     SI
         DEC     AX
         JNE     COMPB
@@ -117,25 +126,27 @@ CAL:    MOV     BX, NUM     ; 计算目标学生下标值, 存放至 BX
         SUB     BX, CX
         IMUL    BX, 14      ; 根据目标学生下标值, 找到分数缓冲区首地址
         ADD     BX, 10      ; BX = 0 + Index * 14 + 10
+        MOV     AX, 0
+        MOV     DX, 0
         MOV     AL, [BX]    ; 计算平均成绩
-        ADD     AL, AL      ; AL = ZH * 2
-        ADD     AL, [BX + 1]; AL = ZH * 2 + MA
+        ADD     AX, AX      ; AL = ZH * 2
+        MOV     DL, [BX + 1]
+        ADD     AX, DX      ; AL = ZH * 2 + MA
         MOV     DL, [BX + 2]
         SAR     DL, 1
-        ADD     AL, DL      ; AL = ZH * 2 + MA + EN / 2
-        SAL     AL, 2       ; AL = 2 * AL
-        MOV     AH, 0
+        ADD     AX, DX      ; AL = ZH * 2 + MA + EN / 2
+        SAL     AX, 1       ; AL = 2 * AL
         MOV     DX, 7
-        IDIV    DX          ; AL = AL / 7
+        IDIV    DL          ; AL = AL / 7
         MOV     [BX + 3], AL; AVG = AL ( AL / 3.5)
-        CMP     AX, 90      ; switch 语句
-        JBE     GRADEA
-        CMP     AX, 80
-        JBE     GRADEB
-        CMP     AX, 70
-        JBE     GRADEC
-        CMP     AX, 60
-        JBE     GRADED
+        CMP     AL, 90      ; switch 语句
+        JGE     GRADEA
+        CMP     AL, 80
+        JGE     GRADEB
+        CMP     AL, 70
+        JGE     GRADEC
+        CMP     AL, 60
+        JGE     GRADED
         JMP     GRADEE
 
 OVER:   MOV     AH, 4CH
