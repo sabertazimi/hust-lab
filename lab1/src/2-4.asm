1774fcba01ef1e4c2db5b35882153fd0117e602b
sabertazimi <sabertazimi@gmail.com>
2016-03-21 23:03:41 +0800

    Fix(Lab 1): buf -> offset buf

--- lab1/src/2-4.asm
+++ lab1/src/2-4.asm
@@ -2,12 +2,12 @@ START:  MOV     AX, DATA
         MOV     DS, AX
         MOV     SI, 0
         MOV     CX, 10
-LOPA:   MOV     AL, [SI + BUF1]
-        MOV     [SI + BUF2], AL
+LOPA:   MOV     AL, [SI + OFFSET BUF1]
+        MOV     [SI + OFFSET BUF2], AL
         INC     AL
-        MOV     [SI + BUF3], AL
+        MOV     [SI + OFFSET BUF3], AL
         ADD     AL, 3
-        MOV     [SI + BUF4], AL
+        MOV     [SI + OFFSET BUF4], AL
         INC     SI
         DEC     CX
         JNZ     LOPA
