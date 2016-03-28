c0fb4b7358c2d47c17f8c038d09b1b0b5f2fe527
sabertazimi <sabertazimi@gmail.com>
2016-03-28 20:41:06 +0800

    Update(Lab 3): preliminarily complete lab 3
    Have fixed two bugs with td.

--- lab3/docs/lab3.md
+++ lab3/docs/lab3.md
@@ -10,3 +10,33 @@
 
 -   'lancer0000' -> 'lancer', 4 DUP(0)
 -   'saber00000' -> 'saber', 5 DUP(0)
+
+### 意外 - 取出目标学生平均成绩
+
+```asm
+mov bx, num
+sub bx, cx
+imul bx, 14
+add bx, 10
+mov al, [bx + 3]
+```
+
+```asm
+mov bx, num
+sub bx, cx
+imul bx, 14
+add bx, 13
+mov al, [bx + 3]
+```
+
+```asm
+mov bx, num
+sub bx, cx
+imul bx, 14
+add bx, 13
+mov al, [bx]
+```
+
+自做聪明将`add bx, 10`改为`add bx, 13`,结果忽视了`mov al, [bx+3]`,将原来正确的代码改出了bug
+
+解决办法： 一不做，二不休，将`mov al, [bx + 3]`改为`mov al, [bx]`
--- lab3/src/grade.asm
+++ lab3/src/grade.asm
@@ -6,35 +6,20 @@ STACK   ENDS
 
 
 DATA    SEGMENT     USE16
-        NUM     EQU 3
-        BUF     DB  'saber'
-        LEN1    EQU $-BUF
-        ZLEN1   EQU 10-LEN1
-                DB  ZLEN1 DUP(0)
-        ZH      DB  90
-        MA      DB  80
-        EN      DB  85
-        AVG     DB  ?
-        ARCH    DB  'archer'
-        LEN2    EQU $-ARCH
-        ZLEN2   EQU 10-LEN2
-                DB  ZLEN2 DUP(0)
-                DB  80
-                DB  75
-                DB  85
-                DB  ?
-        LANC    DB  'lancer'
-        LEN3    EQU $-LANC
-        ZLEN3   EQU 10-LEN3
-                DB  ZLEN3 DUP(0)
+        NUM     EQU 100
+        BUF     DB  99 DUP('lancer', 4 DUP(0), 90, 90, 90 , ?)
+        LAST    DB  'saber'
+                DB  5 DUP(0)
                 DB  70
                 DB  80
-                DB  75
+                DB  90
                 DB  ?
-        MSG     DB  'Please input target name (end with $): $'
+        MSG     DB  'Please input target name: $'
         CONTINU DB  'Enter any keys to continue(except q):$'
         FAIL    DB  'Not Exist!$'
-        INPUT   DB  10 DUP(10)
+        INPUT   DB  10
+                DB  ?
+                DB  10 DUP(0)
 DATA    ENDS
 
 
@@ -43,7 +28,27 @@ CODE    SEGMENT     USE16
 
 START:  MOV     AX, DATA
         MOV     DS, AX
-        JMP     READ
+        MOV     CX, NUM
+CAL:    MOV     BX, NUM     ; 计算目标学生下标值, 存放至 BX
+        SUB     BX, CX
+        IMUL    BX, 14      ; 根据目标学生下标值, 找到分数缓冲区首地址
+        ADD     BX, 10      ; BX = 0 + Index * 14 + 10
+        MOV     AX, 0       ; 初始化寄存器，使得代码上下文无关，减少bug
+        MOV     DX, 0       ; 初始化寄存器，使得代码上下文无关，减少bug
+        MOV     AL, [BX]    ; 计算平均成绩
+        ADD     AX, AX      ; AL = ZH * 2
+        MOV     DL, [BX + 1]
+        ADD     AX, DX      ; AL = ZH * 2 + MA
+        MOV     DL, [BX + 2]
+        SAR     DL, 1
+        ADD     AX, DX      ; AL = ZH * 2 + MA + EN / 2
+        SAL     AX, 1       ; AL = 2 * AL
+        MOV     DX, 7
+        IDIV    DL          ; AL = AL / 7
+        MOV     [BX + 3], AL; AVG = AL ( AL / 3.5)
+        DEC     CX
+        JE      READ        ; 计算完100个学生，跳转至用户输入
+        JMP     CAL         ; 未计算完100个学生，继续计算
 
 GRADEA: MOV     DL, 41H
         MOV     AH, 2H
@@ -65,7 +70,6 @@ GRADEE: MOV     DL, 45H
         MOV     AH, 2H
         INT     21H
         JMP     READY
-
 
 FAILED: LEA     DX, OFFSET FAIL     ; 输出提示信息 - 查找失败
         MOV     AH, 9H
@@ -98,47 +102,34 @@ READ:   MOV     CX, NUM     ; 学生个数
         MOV     DL, 0AH     ; 输出换行符
         MOV     AH, 2H
         INT     21H
-        LEA     DX, OFFSET INPUT
-        MOV     AH, 9H
-        INT     21H
-
-        LEA     BP, OFFSET INPUT; 将 INPUT 基址存放至 BP
-        ADD     BP,2        ; FIX ME:如果不+2修正,则取不到输入字符串,一定是我哪里写错了
+        ; LEA     DX, OFFSET INPUT
+        ; MOV     AH, 9H
+        ; INT     21H
+        LEA     BP, OFFSET INPUT   ; 将 INPUT 基址存放至 BP
+        ADD     BP,2               ; 取出用户输入姓名
         INC     CX
 COMPA:  DEC     CX
         JE      FAILED      ; 查找失败, 重新输入
         MOV     BX, NUM     ; 计算目标学生下标值, 存放至 BX
         SUB     BX, CX
-        IMUL    BX, 14      ; 根据目标学生下标值, 找到分数缓冲区首地址
+        IMUL    BX, 14      ; 根据目标学生下标值, 找到姓名缓冲区首地址
         MOV     AX, 10      ; 临时计数器
         MOV     SI, 0
 COMPB:  MOV     DL, [BX + SI]
         MOV     DH, BYTE PTR DS:[BP + SI]
         CMP     DL, 0       ; 如果缓冲区姓名已结束,说明查找成功
-        JE      CAL         ; 跳转至平均成绩计算处
+        JE      GRADE       ; 跳转至平均成绩计算处
         CMP     DH, DL      ; 比较 当前缓冲区姓名 与 输入姓名 字符
         JNE     COMPA       ; 当前字符相同,继续循环以比较下一字符
         INC     SI
         DEC     AX
         JNE     COMPB
 
-CAL:    MOV     BX, NUM     ; 计算目标学生下标值, 存放至 BX
+GRADE:  MOV     BX, NUM     ; 计算目标学生下标值, 存放至 BX
         SUB     BX, CX
-        IMUL    BX, 14      ; 根据目标学生下标值, 找到分数缓冲区首地址
-        ADD     BX, 10      ; BX = 0 + Index * 14 + 10
-        MOV     AX, 0
-        MOV     DX, 0
-        MOV     AL, [BX]    ; 计算平均成绩
-        ADD     AX, AX      ; AL = ZH * 2
-        MOV     DL, [BX + 1]
-        ADD     AX, DX      ; AL = ZH * 2 + MA
-        MOV     DL, [BX + 2]
-        SAR     DL, 1
-        ADD     AX, DX      ; AL = ZH * 2 + MA + EN / 2
-        SAL     AX, 1       ; AL = 2 * AL
-        MOV     DX, 7
-        IDIV    DL          ; AL = AL / 7
-        MOV     [BX + 3], AL; AVG = AL ( AL / 3.5)
+        IMUL    BX, 14      ; 根据目标学生下标值, 找到平均分数缓冲区首地址
+        ADD     BX, 13      ; BX = 0 + Index * 14 + 13
+        MOV     AL, [BX]; 取出平均成绩
         CMP     AL, 90      ; switch 语句
         JGE     GRADEA
         CMP     AL, 80
