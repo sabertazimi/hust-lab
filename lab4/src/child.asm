2d6dd5223d4703dbfb4c4e13472f2abe53e76076
sabertazimi <sabertazimi@gmail.com>
2016-04-10 21:50:52 +0800

    Update(Merge uncertainmove): merge two modules into one

--- lab4/src/child.asm
+++ lab4/src/child.asm
@@ -15,7 +15,11 @@ data  segment use16   para    public  'data'
                         db  "2. Octal", 0ah, 0dh
                         db  "3. Decimal", 0ah, 0dh
                         db  "4. Hexadecimal$", 0ah, 0dh
-        c_tmp_grade     db  "tmp$"
+        c_choose        db  ?
+        c_null          db  '  $'
+        c_stu_name      db  '**student name:$'
+        c_stu_grade     db  '**grade:$'
+        c_tmp_grade     db  5 dup("tmp$")
 data ends
 
 code    segment use16   para    public  'code'
@@ -32,25 +36,60 @@ c_crlf  macro
         pop     edx                     ; 以下为恢复现场
         pop     eax
         endm
+; 9号dos功能调用
+c_dos9  macro   str
+        push    eax
+        push    edx                     ;以上为保护现场
+        lea     dx, str
+        mov     ah, 9
+        int     21h
+        pop     edx                     ;以下为恢复现场
+        pop     eax
+        endm
+; 10号功能调用
+c_dos10 macro   adr
+        push    eax
+        push    edx
+        lea     dx, adr
+        mov     ah, 10
+        int     21h
+        pop     edx
+        pop     eax
+        endm
+; 1号功能调用
+c_dos1 macro    c_choose
+        push    eax
+        push    edx
+        mov     ah, 1
+        int     21h
+        mov     c_choose, al
+        c_crlf
+        pop     edx
+        pop     eax
+        endm
 
 ; 函数名称: c_exchange
 ; 交换学生成绩表中两个学生的位置
 ; 入口参数: esi, edi 分别为两个学生信息的偏移地址
 ; 出口参数: 无
+; 所使用寄存器：
+; al - 用于交换信息
 c_exchange  proc    far
-            push    ecx
-            push    edx
-            xor     ecx, ecx            ; 计数器初始化为0
-c_exchange_loop:
-            mov     dl, [esi + ecx]
-            xchg    dl, [edi + ecx]
-            mov     [esi + ecx], dl     ; 逐字符交换两个学生的信息
-            inc     ecx
-            cmp     ecx, 0eh
-            jne     c_exchange_loop     ; 交换14个字节为止
-            pop     edx
-            pop     ecx
-            ret
+                push    eax
+                push    ebx
+                push    ecx                         ;以上为保护现场
+                mov     ecx, 0
+c_exchange_loop1:
+                mov     al, [esi][ecx]
+                xchg    al, [edi][ecx]
+                mov     [esi][ecx], al                ;交换 
+                inc     ecx
+                cmp     ecx, 14
+                jne     c_exchange_loop1
+                pop     ecx                         ;以下为恢复现场
+                pop     ebx
+                pop     eax
+                ret            
 c_exchange  endp
 
 ; 函数名称: c_sort
@@ -58,45 +97,25 @@ c_exchange  endp
 ; 入口参数: 无
 ; 出口参数: 无
 c_sort      proc    far
-            push    eax
-            push    ebx
-            push    ecx
-            push    edx
-            push    esi
-            push    edi
-            xor     ebx, ebx
-            xor     ecx, ecx
-            xor     edx, edx
-            ;       冒泡排序算法
-            xor     eax, eax            ; 外循环计数器
-c_sort_outer:
-            cmp     eax, m_num
-            jge     c_sort_finish
-            imul    ebx, eax, 14
-            lea     esi, [ebx + m_table]; avg[i]
-            lea     ecx, [eax + 1]      ; 内循环计数器
-c_sort_inner:
-            cmp     ecx, m_num
-            jge     c_sort_outer_next
-            imul    ebx, ecx, 14
-            lea     edi, [ebx + m_table]; avg[j]
-            mov     dl, [esi + 13]
-            cmp     [edi + 13], dl
-            jge     c_sort_inner_next
-            call    c_exchange          ; 若　avg[j] < avg[i], 交换两者信息
-c_sort_inner_next:
-            inc     ecx                 ; 内循环计数器自增
-            jmp     c_sort_inner
-c_sort_outer_next:
-            inc     eax
-            jmp     c_sort_outer
-c_sort_finish:
-            pop     edi
-            pop     esi
-            pop     edx
-            pop     ecx
-            pop     ebx
-            pop     eax
+        
+                                            ; 以上为保护现场
+            mov     ecx, m_num         ; 外循环计数器
+            mov     edx, offset m_table
+c_sort_loop1:
+            mov     ebx, 1                  ; 内循环计数器
+c_sort_loop2:
+            imul    eax, ebx, 14
+            mov     dl, byte ptr m_table[eax - 1]
+            cmp     dl, byte ptr m_table[eax + 13]
+            jge    c_sort_loop3
+            lea     esi, byte ptr m_table[eax - 14]
+            lea     edi, byte ptr m_table[eax]
+            call    far ptr c_exchange              ;交换学生位置
+c_sort_loop3:
+            inc     ebx
+            cmp     ebx, m_num
+            jnz     c_sort_loop2
+            loop    c_sort_loop1
             ret
 c_sort      endp
 
@@ -110,7 +129,7 @@ c_sort      endp
 ; si - 指向字节缓冲区中最后一个 ascii 码的下一个字节处
 ; 寄存器:
 ; cx - P进制数字入栈／出栈时的计数器
-; edx - 作除法时存放被除数高位或余数
+; edx - 作法时存放被除数高位或余数除
 c_radix     proc
             push    cx
             push    edx
@@ -132,90 +151,78 @@ c_radix_l1:
             mov     [si], al        ; 送入缓冲区
             inc     si
             loop    c_radix_loop2   ; (cx)--
+            mov     byte ptr [si], '$'       ;写入结束符
+            inc     si
             pop     edx
             pop     cx
             ret
 c_radix     endp
+
+; 函数名称：c_print_name
+; 打印学生名字
+; 入口参数：
+; edi - 学生信息的偏移地址
+; 出口参数：无
+c_print_name     proc
+                push    ecx
+                push    ebx
+                mov     ecx, 0
+c_print_name_loop:
+                mov     dl, [edi][ecx]
+                mov     ah, 2
+                int     21h
+                inc     ecx
+                cmp     byte ptr [edi][ecx], 0
+                jne     c_print_name_loop
+                c_dos9  c_null
+                pop     ebx
+                pop     ecx
+                ret
+c_print_name    endp
 
 ; 函数名称: c_print
 ; 打印学生成绩表
 ; 入口参数: 无
 ; 出口参数: 无
 c_print     proc    far
-            push    eax
-            push    ebx
-            push    ecx
-            push    edx
-            push    esi
-            push    edi                     ; 以上为保护现场
-            c_crlf
-            lea     dx, c_print_promt       ; 输出菜单
-            mov     ah, 9h
-            int     21h
-            c_crlf
-            mov     ah, 1h                  ; 用户输入所需功能 - 选择以何种进制显示成绩
-            int     21h
-            c_crlf
-            cmp     al, 31h                 ; 用户选择 2 进制
-            jne     c_print_l1
-            mov     ebx, 2h
-            jmp     c_print_next
-c_print_l1:
-            cmp     al, 32h                 ; 用户选择 8 进制
-            jne     c_print_l2
-            mov     ebx, 8h
-            jmp     c_print_next
-c_print_l2:
-            cmp     al, 33h                 ; 用户选择 10 进制
-            jne     c_print_l3
-            mov     ebx, 0ah
-            jmp     c_print_next
-c_print_l3:
-            cmp     al, 34h                 ; 用户选择 16 进制
-            jne     c_print_next
-            mov     ebx, 10h
-c_print_next:
-            xor     ecx, ecx                ; 计数器初始化为0
-c_print_loop:
-            cmp     ecx, m_num
-            jge     c_print_finish
-            imul    esi, ecx, 14
-            lea     edi, [esi + m_table]    ; 按照下表取出目标学生信息的偏移地址
-            push    ecx                     ; 保护外循环计数器
-            xor     ecx, ecx                ; 打印学生姓名,计数器初始化为0
-c_print_name:
-            mov     dl, [edi + ecx]         ; 依次打印学生姓名各个字符
-            mov     ah, 2h
-            int     21h
-            inc     ecx
-            cmp     ecx, 10
-            jne     c_print_name            ; 打印完10个字符为止
-            xor     ecx, ecx                ; 打印学生成绩,计数器初始化为0
-c_print_grade:
-            xor     eax, eax
-            mov     al, [edi + ecx + 10]    ; 取出学生成绩
-            lea     si, c_tmp_grade         ; 用于存放转化结果的缓冲区
+            c_dos9      c_print_promt   ;9号功能调用
+            c_dos1     c_choose
+            mov     bl, c_choose
+c_print_loop1:
+            cmp     bl, '1'
+            jne     c_print_loop2
+            mov     ebx, 2
+            jmp     c_print_loop5
+c_print_loop2:
+            cmp     bl, '2'
+            jne     c_print_loop3
+            mov     ebx, 8
+            jmp     c_print_loop5
+c_print_loop3:
+            cmp     bl, '3'
+            jne     c_print_loop4
+            mov     ebx, 10
+            jmp     c_print_loop5
+c_print_loop4:
+            cmp     bl, '4'
+            jne     c_print_loop5
+            mov     ebx, 16
+            jmp     c_print_loop5
+c_print_loop5:
+            mov     ecx, m_num      ;循环记数
+            lea     edi, m_table        ;学生信息的偏移地址
+c_print_loop6:
+            lea     si, c_tmp_grade
+            c_dos9  c_stu_name
+            call    c_print_name
+            mov     al, [edi + 13]
+            movzx   eax, al          ;扩展为32位无符号二进制数
             call    c_radix
-            lea     dx, c_tmp_grade         ; 打印转化后的成绩数字串
-            mov     byte ptr [si], 20h
-            mov     byte ptr [si + 1], 20h  ; 成绩之间用空格分隔
-            mov     byte ptr [si + 2], '$'  ; 形成字符串
-            mov     ah, 9h
-            int     21h
-            inc     ecx
-            cmp     ecx, 4
-            jne     c_print_grade           ; 打印 3 + 1　门成绩为止
-            pop     ecx                     ; 恢复外循环计数器
-            c_crlf                          ; 换行
-            inc     ecx
-            jmp     c_print_loop
-c_print_finish:
-            pop     edi                     ; 以下为恢复现场
-            pop     esi
-            pop     edx
-            pop     ecx
-            pop     ebx
-            pop     eax
+            c_dos9  c_stu_grade
+            c_dos9  c_tmp_grade
+            c_crlf
+            add     edi, 14
+            loop    c_print_loop6
             ret
 c_print     endp
 
