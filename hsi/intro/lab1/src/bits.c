58b1b666d937f2752282f514e45154d44dce03d3
sabertazimi <sabertazimi@gmail.com>
2016-04-20 22:02:43 +0800

    Refactor(HSI-Lab1): move lab materials provided by ta into intro/

--- hsi/intro/lab1/src/bits.c
+++ hsi/intro/lab1/src/bits.c
@@ -246,11 +246,12 @@ int addOK(int x, int y) {
  *   Rating: 4
  */
 int bang(int x) {
-  int minus_x = ~x+1;
+    int minus_x = ~x+1;
   /* Cute trick: 0 is the only value of x
    * for which neither x nor -x are negative */
-  return (~(minus_x|x) >> 31) & 1;
+    return (~(minus_x|x) >> 31) & 1;
 }
+
 /*
  * conditional - same as x ? y : z
  *   Example: conditional(2,4,5) = 4
