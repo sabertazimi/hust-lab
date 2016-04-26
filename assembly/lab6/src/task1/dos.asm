fcf276c5ac9636672b95ea066401b5cb7f825aa1
sabertazimi <sabertazimi@gmail.com>
2016-04-26 20:19:05 +0800

    Update(Asm-Lab6-Docs): Complete lab 6 document

--- assembly/lab6/src/task1/dos.asm
+++ assembly/lab6/src/task1/dos.asm
@@ -2,9 +2,7 @@
 
 code    segment use16   para    public  'code'
         assume  cs: code
-start:  
-        push    cs
-        push    ds
+start:
         mov     ax, 3510h   ; 获取10h的中断矢量
         int     21h         ; 段址保存在 es, 偏移址保存在 bx
         mov     ah, 4ch
