5718b31633c13940317b21807b53cb7f0334df1b
sabertazimi <sabertazimi@gmail.com>
2016-04-26 15:55:26 +0800

    Update(

--- assembly/lab6/src/task2/int.asm
+++ assembly/lab6/src/task2/int.asm
@@ -1,65 +1,66 @@
 .386
 
 code    segment use16
-        assume  cs:code,ds:code,ss:stack
+        assume  cs:code,ss:stack
 
 old_int dw  ?, ?
 
-new_16h proc
-        ; 手动调用系统原有键盘驱动程序,处理键盘输入
-        pushf           ; 标志寄存器压栈
-        cli             ; 关中断
-        ; 关闭单步异常
+new_16h:
+        ; 查看用户是否调用键盘驱动程序的读入键盘功能
+        cmp ah, 0h
+        je input_0 
+        cmp ah, 10h
+        je input_0
+        ; 若不是,则直接跳转至旧的键盘驱动程序
+        jmp dword ptr old_int
+        ; 否则,运行扩展程序
+input_0:
         pushf
-        pop ax          ; 将标志位送入ax
-        and ah, 0feh    ; 关单步异常(tf置0)
-        push ax
-        popf            ; 将修改后的标志位送入标志寄存器
-        ; 设置返回地址
-        push    cs      ; 段首址压栈
-        mov     ax, offset a_input
-        push    ax      ; ip值压栈
-        ; 跳转至系统原有键盘驱动程序
-        mov ah, 0h
-        jmp     dword ptr old_int
-a_input:
-        cmp al, 41h     ; 若按下字母A,则输出B
-        jne b_input
-        mov dl, 42h
-        mov ah, 2h
-        int 21h         ; 输出B
-b_input:
-        cmp al, 42h     ; 若按下字母B,则输出A
+        ; 调用旧的键盘驱动程序
+        call dword ptr old_int
+        cmp al, 'a'     ; 若按下字母A,则返回B
+        jne input_1
+        mov al, 'b';
+        jmp finish
+input_1:
+        cmp al, 'A'
+        jne input_2
+        mov al, 'B'
+        jmp finish
+input_2:
+        cmp al, 'b'     ; 若按下字母B,则返回A
+        jne input_3
+        mov al, 'a'
+        jmp finish
+input_3:
+        cmp al, 'B'     ; 若按下字母B,则返回A
         jne finish
-        mov dl, 41h
-        mov ah, 2h
-        int 21h         ; 输出A
+        mov al, 'A'
 finish:
-        iret
-new_16h endp
-
+        iret 
 ; 初始化(安装新中段矢量并使其常驻内存)
 start:  
-        push    cs
-        push    ds
-        mov     ax, 3516h           ; 获取16h的中断矢量
-        int     21h                 ; 段址保存在 es, 偏移址保存在 bx
-        mov     old_int, bx         ; 保存旧程序偏移址
-        mov     old_int+2, es       ; 保存旧程序段首址
+        xor     ax, ax
+        mov     ds, ax
+        mov     ax, ds:[16h*4]
+        mov     old_int, ax
+        mov     ax, ds:[16h*4+2]
+        mov     old_int+2, ax
+        ; mov     ax, 3516h           ; 获取16h的中断矢量
+        ; int     21h                 ; 段址保存在 es, 偏移址保存在 bx
+        ; mov     old_int, bx         ; 保存旧程序偏移址
+        ; mov     old_int+2, es       ; 保存旧程序段首址
         ; 修改中断矢量表
-        mov     dx, offset new_16h
-        mov     ax, 2516h
-        int     21h
-        ; 修改中断矢量表
-        ; xor     ax, ax
-        ; mov     ds, ax
-        ; cli
-        ; mov     word ptr ds:[58h], offset new_16h
-        ; mov     ds:[5ah], cs
-        ; sti
+        ; push  cs
+        ; pop   ds
         ; mov     dx, offset new_16h
         ; mov     ax, 2516h
         ; int     21h
+        ; 修改中断矢量表
+        cli
+        mov     word ptr ds:[58h], offset new_16h
+        mov     ds:[5ah], cs
+        sti
         ; 驻留内存
         mov     dx, offset start + 15
         mov     cl, 4
