4f794c57c22f20719838ef9d34043f895610c01b
sabertazimi <sabertazimi@gmail.com>
2016-05-21 18:16:41 +0800

    Add(Verilog-Lab2_1): Have a try on 7 segment decoder

--- verilog/lab2/src/lab2_1_bcd_to_7segment_decoder.v
+++ verilog/lab2/src/lab2_1_bcd_to_7segment_decoder.v
@@ -5,6 +5,6 @@ module bcd_to_7segment_decoder(input [3:0] x, output [3:0] an, output [6:0] seg)
 	assign  seg[2] = ~x[0] & x[1] & ~x[2]; // c
 	assign  seg[3] = (x[0] & ~x[1] & ~x[2]) | (x[0] ~^ x[1] & x[2]); // d
 	assign  seg[4] = (x[0] & ~x[2]) | (~x[1] & x[2]); // e
-	assign  seg[5] = ; // f
-	assign  seg[6] = ; // g
+	assign  seg[5] = (x[0] & x[1]) | (x[1] & ~x[2]) | (x[0] & ~x[2] & ~x[3]); // f
+	assign  seg[6] = (x[0] & x[1] & x[2]) | (~x[1] & ~x[2] & ~x[3]); // g
 endmodule
