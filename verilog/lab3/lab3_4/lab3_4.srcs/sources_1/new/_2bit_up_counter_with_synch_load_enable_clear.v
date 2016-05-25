d64d189a80ad95bc329305a7a3c727ddc361541d
sabertazimi <sabertazimi@gmail.com>
2016-05-25 10:47:57 +0800

    Add(Verilog-Lab3): Complete self-defined design - mod 4 counter

---
+++ verilog/lab3/lab3_4/lab3_4.srcs/sources_1/new/_2bit_up_counter_with_synch_load_enable_clear.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2016/05/25 10:33:11
+// Design Name: 
+// Module Name: _2bit_up_counter_with_synch_load_enable_clear
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+// mod 4 adder
+
+module _2bit_up_counter_with_synch_load_enable_clear(
+    input Clock,
+input Clear,
+input Enable,
+input Load,
+output [1:0] Q
+);
+
+reg [1:0] count;
+
+assign Q = count;
+
+always @(posedge Clock) begin
+    if (Clear) count <= 2'b00;
+    else if (Enable)
+        if (Load && count == 2'b11) count <= 2'b00;
+        else count <= count + 1;
+end
+endmodule
