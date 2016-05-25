5efd735c296cc87004f6dac45a33a5257a3b8e67
sabertazimi <sabertazimi@gmail.com>
2016-05-25 09:40:11 +0800

    Add(Verilog-Lab3): Complete 8 bit counters with T flip-flops design

--- verilog/lab3/lab3_2_2_1/lab3_2_2_1.srcs/sources_1/new/counters_8bit_with_T_ff.v
+++ verilog/lab3/lab3_2_2_1/lab3_2_2_1.srcs/sources_1/new/counters_8bit_with_T_ff.v
@@ -24,6 +24,15 @@ module counters_8bit_with_T_ff(
     input Clk,
     input Enable,
     input Clear,
-    output 
+    output [7:0] Q
     );
+    
+    T_ff_with_synch_reset_behavior SR0 (.Clk(Clk), .T(Enable), .reset_n(Clear), .Q(Q[0]));
+    T_ff_with_synch_reset_behavior SR1 (.Clk(Clk), .T(Enable & Q[0]), .reset_n(Clear), .Q(Q[1]));
+    T_ff_with_synch_reset_behavior SR2 (.Clk(Clk), .T(Enable & Q[0] & Q[1]), .reset_n(Clear), .Q(Q[2]));
+    T_ff_with_synch_reset_behavior SR3 (.Clk(Clk), .T(Enable & Q[0] & Q[1] & Q[2]), .reset_n(Clear), .Q(Q[3]));
+    T_ff_with_synch_reset_behavior SR4 (.Clk(Clk), .T(Enable & Q[0] & Q[1] & Q[2] & Q[3]), .reset_n(Clear), .Q(Q[4]));
+    T_ff_with_synch_reset_behavior SR5 (.Clk(Clk), .T(Enable & Q[0] & Q[1] & Q[2] & Q[3] & Q[4]), .reset_n(Clear), .Q(Q[5]));
+    T_ff_with_synch_reset_behavior SR6 (.Clk(Clk), .T(Enable & Q[0] & Q[1] & Q[2] & Q[3] & Q[4] & Q[5]), .reset_n(Clear), .Q(Q[6]));
+    T_ff_with_synch_reset_behavior SR7 (.Clk(Clk), .T(Enable & Q[0] & Q[1] & Q[2] & Q[3] & Q[4] & Q[5] & Q[6]), .reset_n(Clear), .Q(Q[7]));
 endmodule
