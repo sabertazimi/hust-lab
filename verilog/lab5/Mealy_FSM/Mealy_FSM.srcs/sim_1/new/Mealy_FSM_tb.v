9d48790f93ed714f053e730df615ac8f03d6fa67
sabertazimi <sabertazimi@gmail.com>
2016-06-06 21:10:02 +0800

    Add(Verilog-Lab5): Complete task 1(pdf 1) - FSM
    FSM type: Mealy_FSM , Moore_FSM, Mealy_FSM with ROM

--- verilog/lab5/Mealy_FSM/Mealy_FSM.srcs/sim_1/new/Mealy_FSM_tb.v
+++ verilog/lab5/Mealy_FSM/Mealy_FSM.srcs/sim_1/new/Mealy_FSM_tb.v
@@ -23,4 +23,40 @@
 module Mealy_FSM_tb(
 
     );
+    
+    reg Clk, reset, ain;
+    wire [3:0] count;
+    wire yout;
+    integer i;
+    
+    Mealy_FSM DUT (Clk, reset, ain, count, yout);
+    
+    initial begin
+        #330 $finish;
+    end
+    
+    initial begin
+        Clk = 0;
+        for (i = 0; i < 66;i = i + 1) begin
+            #5 Clk = ~Clk;
+        end
+    end
+    
+    initial begin
+        reset = 1;
+        #20 reset = 0;
+        #170 reset = 1;
+        #10 reset = 0;
+    end
+    
+    initial begin
+        ain = 0;
+        #40 ain = 1;
+        #20 ain = 0;
+        #60 ain = 1;
+        #40 ain = 0;
+        #20 ain = 1;
+        #30 ain = 0;
+        #30 ain = 1;
+    end
 endmodule
