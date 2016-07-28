6d6ca6c01f130d461e85b8d82a47de1b91462b42
sabertazimi <sabertazimi@gmail.com>
2016-07-28 13:21:25 +0800

    update(csapp-malloc): implement the simplest malloctor

--- csapp/malloclab/mm.h
+++ csapp/malloclab/mm.h
@@ -1,3 +1,6 @@
+#ifndef __MM_H__
+#define __MM_H__
+
 #include <stdio.h>
 
 extern int mm_init (void);
@@ -21,3 +24,28 @@ typedef struct {
 
 extern team_t team;
 
+/* Begin of macro */
+
+#define WSIZE 4
+#define DSIZE 8
+#define CHUNKSIZE           (1 << 12)
+
+#define MAX(X, y)           ((x) > (y) ? (x) : (y))
+
+#define PACK(size, alloc)   ((size) | (alloc))
+
+#define GET(p)              (*(unsigned int *)(p))
+#define PUT(p, val)         (*(unsigned int *)(p) = (val))
+
+#define GET_SIZE(p)         (GET(p) & ~0x7)
+#define GET_ALLOC(p)        (GET(p) & 0x1)
+
+#define HDRP(bp)            ((char *)(bp) - WSIZE)
+#define FTRP(bp)            ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+#define NEXT_BLKP(bp)       ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp)       ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
+/* End of macro */
+
+#endif __MM_H__
