651af2fa091cd8233bf17c73f7373a879f9684a0
sabertazimi <sabertazimi@gmail.com>
2016-08-13 09:58:19 +0800

    update(compilers-cminus-lab7): complete semantic analysis
    Complete simple symbol table and type checking.There's no scope checking
    lab.
    
    issue #25

--- compilers/cminus/lab7/main.c
+++ compilers/cminus/lab7/main.c
@@ -2,27 +2,35 @@
 #include "ast.h"
 #include "semant.h"
 
+#define DEBUG
+#undef DEBUG
+
 extern void yyparse();
 extern Prog_t tree;
 
-int main (int argc, char **argv)
-{
-  printf ("lex and parse starting...\n");
+int main (int argc, char **argv) {
+  printf("lex and parse starting...\n");
   int v;
-  /*
-  while ((v=yylex())!=EOF)
-    printf ("%d\n", v);
-  printf ("%d\n", v);
-  */
-  yyparse();
-  printf ("lex and parse finished\n");
-  
-  printf ("print the AST starting...\n");
-  Prog_print (tree);
-  printf ("print the AST finished\n");
 
-  printf ("semantic analysis starting...\n");
+#ifdef DEBUG
+
+  while ((v = yylex()) != EOF) {
+    printf("%d\n", v);
+  }
+
+  printf("%d\n", v);
+
+#endif
+
+  yyparse();
+  printf("lex and parse finished\n\n");
+
+  printf("print the AST starting...\n");
+  Prog_print(tree);
+  printf("print the AST finished\n\n");
+
+  printf("semantic analysis starting...\n");
   Semant_check(tree);
-  printf ("semantic analysis finished\n");
+  printf("semantic analysis finished\n\n");
   return 0;
 }
