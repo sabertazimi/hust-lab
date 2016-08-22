8f25d9f2a365e16112c026d4afc4c39b49ea6b99
sabertazimi <sabertazimi@gmail.com>
2016-08-22 14:52:07 +0800

    udpate(os-ucore-lab2): read the source code of transfrom macro
    Master the macro about the transfrom between physical address, physical
    page number, page pointer, page table entry, page directory entry
    
    issue sabertazimi/awesome-notes#27

--- operatingSystems/ucore/labs/lab2/kern/mm/pmm.h
+++ operatingSystems/ucore/labs/lab2/kern/mm/pmm.h
@@ -13,13 +13,13 @@
 struct pmm_manager {
     const char *name;                                 // XXX_pmm_manager's name
     void (*init)(void);                               // initialize internal description&management data structure
-                                                      // (free block list, number of free block) of XXX_pmm_manager 
+                                                      // (free block list, number of free block) of XXX_pmm_manager
     void (*init_memmap)(struct Page *base, size_t n); // setup description&management data structcure according to
-                                                      // the initial free physical memory space 
-    struct Page *(*alloc_pages)(size_t n);            // allocate >=n pages, depend on the allocation algorithm 
+                                                      // the initial free physical memory space
+    struct Page *(*alloc_pages)(size_t n);            // allocate >=n pages, depend on the allocation algorithm
     void (*free_pages)(struct Page *base, size_t n);  // free >=n pages with "base" addr of Page descriptor structures(memlayout.h)
-    size_t (*nr_free_pages)(void);                    // return the number of free pages 
-    void (*check)(void);                              // check the correctness of XXX_pmm_manager 
+    size_t (*nr_free_pages)(void);                    // return the number of free pages
+    void (*check)(void);                              // check the correctness of XXX_pmm_manager
 };
 
 extern const struct pmm_manager *pmm_manager;
@@ -74,16 +74,19 @@ void print_pgdir(void);
 extern struct Page *pages;
 extern size_t npage;
 
+// page pointer => physical page number
 static inline ppn_t
 page2ppn(struct Page *page) {
     return page - pages;
 }
 
+// page poiter => physical page number => physical address
 static inline uintptr_t
 page2pa(struct Page *page) {
     return page2ppn(page) << PGSHIFT;
 }
 
+// physical address => physical page number => page pointer
 static inline struct Page *
 pa2page(uintptr_t pa) {
     if (PPN(pa) >= npage) {
@@ -92,16 +95,19 @@ pa2page(uintptr_t pa) {
     return &pages[PPN(pa)];
 }
 
+// page pointer => => physical address => kernel virtual address
 static inline void *
 page2kva(struct Page *page) {
     return KADDR(page2pa(page));
 }
 
+// kernel virtual address => physical address => page pointer
 static inline struct Page *
 kva2page(void *kva) {
     return pa2page(PADDR(kva));
 }
 
+// page table entry => physical address => page pointer
 static inline struct Page *
 pte2page(pte_t pte) {
     if (!(pte & PTE_P)) {
@@ -110,6 +116,7 @@ pte2page(pte_t pte) {
     return pa2page(PTE_ADDR(pte));
 }
 
+// page directory entry => physical address => page pointer
 static inline struct Page *
 pde2page(pde_t pde) {
     return pa2page(PDE_ADDR(pde));
