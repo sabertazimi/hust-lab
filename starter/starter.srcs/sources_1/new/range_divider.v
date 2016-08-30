88167d16ab751dd81ce0db52ef83620085150fac
sabertazimi <sabertazimi@gmail.com>
2016-08-30 11:32:45 +0800

    update(clock-divider): change divider from tick_divider to range_divider
    It can help to improve accuracy of divider.Tick divider only can divide
    clock source into power2.

---
+++ starter/starter.srcs/sources_1/new/range_divider.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2016/08/30 09:01:18
+// Design Name: 
+// Module Name: range_divider
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module range_divider
+#(parameter WIDTH = 32, RANGE = 27'b101111101011110000100000000)
+(
+    input clk_src,
+    output reg clk_dst    
+);
+
+    reg[(WIDTH-1):0] count;
+
+    always@(posedge clk_src) begin
+        count = count + 1;
+        if (count == RANGE) begin
+            count = 0;
+            clk_dst = 1;
+        end else begin
+            clk_dst = 0;
+        end
+    end
+
+endmodule
