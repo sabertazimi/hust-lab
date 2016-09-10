43c57d956d820ad7dc4b678a359a9fe9a885bb56
sabertazimi <sabertazimi@gmail.com>
2016-09-10 10:40:09 +0800

    feat(compilers-lispy): start to complete a lisp interpreter
    issue sabertazimi/awesome-notes#25

---
+++ compilers/lispy/README.md
@@ -0,0 +1,9 @@
+# lispy
+
+A simple lisp interpreter.
+
+## Installation
+
+```sh
+$ sudo apt-get install libedit-dev
+```
---
+++ compilers/lispy/lispy.c
@@ -0,0 +1,13 @@
+/*
+ * lispy.c
+ * Copyright (C) 2016 sabertazimi <sabertazimi@avalon>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "mpc.h"
+
+int main(int argc, char **argv) {
+    printf("lispy > ");
+    return 0;
+}
