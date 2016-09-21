3dbe4232346836d2ea8e637c9db3dd1b02db3520
sabertazimi <sabertazimi@gmail.com>
2016-09-21 15:31:50 +0800

    update(re-installation): install 2016.2 version to start lab

--- wm_components/wm_components.runs/impl_1/selector.tcl
+++ wm_components/wm_components.runs/impl_1/selector.tcl
@@ -45,83 +45,11 @@ proc step_failed { step } {
 set_msg_config -id {HDL 9-1061} -limit 100000
 set_msg_config -id {HDL 9-1654} -limit 100000
 
-start_step init_design
-set rc [catch {
-  create_msg_db init_design.pb
-  set_property design_mode GateLvl [current_fileset]
-  set_param project.singleFileAddWarning.threshold 0
-  set_property webtalk.parent_dir /home/sabertazimi/gitrepo/dld/wm_components/wm_components.cache/wt [current_project]
-  set_property parent.project_path /home/sabertazimi/gitrepo/dld/wm_components/wm_components.xpr [current_project]
-  set_property ip_repo_paths /home/sabertazimi/gitrepo/dld/wm_components/wm_components.cache/ip [current_project]
-  set_property ip_output_repo /home/sabertazimi/gitrepo/dld/wm_components/wm_components.cache/ip [current_project]
-  add_files -quiet /home/sabertazimi/gitrepo/dld/wm_components/wm_components.runs/synth_1/selector.dcp
-  read_xdc /home/sabertazimi/gitrepo/dld/wm_components/wm_components.srcs/constrs_1/new/weight_selector.xdc
-  link_design -top selector -part xc7a100tcsg324-1
-  write_hwdef -file selector.hwdef
-  close_msg_db -file init_design.pb
-} RESULT]
-if {$rc} {
-  step_failed init_design
-  return -code error $RESULT
-} else {
-  end_step init_design
-}
-
-start_step opt_design
-set rc [catch {
-  create_msg_db opt_design.pb
-  opt_design 
-  write_checkpoint -force selector_opt.dcp
-  report_drc -file selector_drc_opted.rpt
-  close_msg_db -file opt_design.pb
-} RESULT]
-if {$rc} {
-  step_failed opt_design
-  return -code error $RESULT
-} else {
-  end_step opt_design
-}
-
-start_step place_design
-set rc [catch {
-  create_msg_db place_design.pb
-  implement_debug_core 
-  place_design 
-  write_checkpoint -force selector_placed.dcp
-  report_io -file selector_io_placed.rpt
-  report_utilization -file selector_utilization_placed.rpt -pb selector_utilization_placed.pb
-  report_control_sets -verbose -file selector_control_sets_placed.rpt
-  close_msg_db -file place_design.pb
-} RESULT]
-if {$rc} {
-  step_failed place_design
-  return -code error $RESULT
-} else {
-  end_step place_design
-}
-
-start_step route_design
-set rc [catch {
-  create_msg_db route_design.pb
-  route_design 
-  write_checkpoint -force selector_routed.dcp
-  report_drc -file selector_drc_routed.rpt -pb selector_drc_routed.pb
-  report_timing_summary -warn_on_violation -max_paths 10 -file selector_timing_summary_routed.rpt -rpx selector_timing_summary_routed.rpx
-  report_power -file selector_power_routed.rpt -pb selector_power_summary_routed.pb -rpx selector_power_routed.rpx
-  report_route_status -file selector_route_status.rpt -pb selector_route_status.pb
-  report_clock_utilization -file selector_clock_utilization_routed.rpt
-  close_msg_db -file route_design.pb
-} RESULT]
-if {$rc} {
-  step_failed route_design
-  return -code error $RESULT
-} else {
-  end_step route_design
-}
-
 start_step write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
+  open_checkpoint selector_routed.dcp
+  set_property webtalk.parent_dir /home/sabertazimi/Work/Source/dld/wm_components/wm_components.cache/wt [current_project]
   catch { write_mem_info -force selector.mmi }
   write_bitstream -force selector.bit 
   catch { write_sysdef -hwdef selector.hwdef -bitfile selector.bit -meminfo selector.mmi -file selector.sysdef }
