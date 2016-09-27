ad30c2a72d0564276ac260b9502e6496ddd86410
sabertazimi <sabertazimi@gmail.com>
2016-09-27 13:01:47 +0800

    report(lab): add timing line record

--- controler/controler.srcs/sources_1/new/water_let_mode.v
+++ controler/controler.srcs/sources_1/new/water_let_mode.v
@@ -50,6 +50,7 @@ module water_let_mode
 
             // TODO: button-down signal led
             // btn_sig_led(bool sig_flicker, output btn_l   ed)
+            // DONE
         end else begin
             init_flag <= 1; water_in_end_sign = 1'b0; water_out_end_sign = 1'b0; water_level = {3{1'b0}};
         end
