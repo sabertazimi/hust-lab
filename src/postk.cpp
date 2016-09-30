ad816f3c0e448b67ef51f6564f60e5891d9f0596
sabertazimi <sabertazimi@gmail.com>
2016-09-30 19:41:27 +0800

    update(postk): start to implement postk struct

--- src/postk.cpp
+++ src/postk.cpp
@@ -8,3 +8,80 @@
  */
 
 #include "postk.h"  // add include/ path to g++ flags
+
+/// \brief initiate stack
+/// \param p stack pointer
+/// \param m stack capacity
+/// \return void
+void initPOSTK(POSTK *const p, int m) {
+
+}
+
+/// \brief initiate stack (with copy)
+/// \param p destination stack pointer
+/// \param s source stack pointer
+/// \return void
+void initPOSTK(POSTK *const p, const POSTK &s) {
+
+}
+
+/// \brief get capacity of stack
+/// \param p stack pointer
+/// \return capacity of stack
+int size(const POSTK *const p) {
+
+}
+
+/// \brief get number of elements in stack
+/// \param p stack pointer
+/// \return number of elements in stack
+int howMany(const POSTK *const p) {
+
+}
+
+/// \brief get target element with index x
+/// \param p stack pointer
+/// \param x index of target element
+/// \return tartget element with index x
+int getelem(const POSTK *const p, int x) {
+
+}
+
+/// \brief push a new element into stack
+/// \param p stack pointer
+/// \param e new element to push
+/// \return stack pointer point to p
+POSTK *const push(POSTK *const p, int e) {
+
+}
+
+/// \brief pop a element from stack
+/// \param p stack pointer
+/// \param e hold value of element poped
+/// \return stack pointer point to p
+POSTK *const pop(POSTK *const p, int &e) {
+
+}
+
+/// \brief assign stack p with stack s
+/// \param p destination stack pointer
+/// \param s source stack pointer
+/// \return stack pointer point to p
+POSTK *const assign(POSTK *const p, const POSTK &s) {
+
+}
+
+/// \brief print all elements in stack
+/// \param p stack pointer
+/// \return void
+void print(const POSTK *const p) {
+
+}
+
+/// \brief destroy stack
+/// \param p stack pointer
+/// \return void
+void destroyPOSTK(POSTK *const p) {
+
+}
+
