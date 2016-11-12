55f9f682c6e471e6adcceea1829bbcf6f500faf7
sabertazimi <sabertazimi@gmail.com>
2016-11-12 16:56:41 +0800

    add(os-shared number): complete shared number operation lab

---
+++ lab2/src/utils/utils.h
@@ -0,0 +1,32 @@
+/*!
+ * \file utils.h
+ * \brief header file for utils.c
+ *
+ * \author sabertazimi, <sabertazimi@gmail.com>
+ * \version 1.0
+ * \date 2016
+ * \license MIT
+ */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+typedef char *string;   ///< alias for char *
+
+/// \brief a simple log function
+/// \param fmt format string
+/// \param ... output string
+/// \return void
+void logger(const char *fmt, ...);
+
+/// \brief LOG: a simple marco encapsulating logger function
+/// \param fmt format string
+/// \param ... output string
+/// \return void
+#ifdef DEBUG
+#define LOG(fmt, ...) do { logger(fmt,##__VA_ARGS__); } while (0)
+#else
+#define LOG(fmt, ...) do { ; } while (0)
+#endif
+
+#endif /* !UTILS_H */
