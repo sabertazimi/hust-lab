11690c54ab548780d43e2f5214e6600f7e3b073c
sabertazimi <sabertazimi@gmail.com>
2016-11-16 23:37:26 +0800

    update(os-lab1): add bug record for pipe+signal lab
    issue sabertazimi/awesome-notes#27

--- docs/report.md
+++ docs/report.md
@@ -1,10 +1,33 @@
-# Lab 1 Notes
+# OS Lab Notes
 
-## Compile and Link
+## Lab 1 Notes
+
+误以为 SIGUSR1 与 SIGUSR2 是由编程者自定义的枚举量
+
+```c
+typedef enum _sigusr_ {
+    SIGUSRONE = 233,
+    SIGUSRTWO = 234
+} sigusr_t;
+
+signal(SIGUSRONE, handler);
+signal(SIGUSRTWO, handler);
+
+kill(c1, SIGUSRONE);
+kill(c2, SIGUSRTWO);
+```
+
+导致父进程发送无效信号, 子进程处理无效信号, 都在做无用功
+
+> solution: SIGUSRONE->SIGUSR1, SIGUSRTWO->SIGUSR2
+
+## Lab 2/3 Notes
+
+### Compile and Link
 
 *   change -lpthread to -pthread
 
-## ipcs and ipcrm
+### ipcs and ipcrm
 
 ```sh
 # show share memory
@@ -24,7 +47,7 @@ $ ipcrm --all=shm
 $ ipcrm --all=sem
 ```
 
-## `key_t` key
+### `key_t` key
 
 对于同一个 key, 在同一进程组中, 调用 semget/shmget 函数所产生的 IPC 信号量为同一个 IPC 信号量:
 
@@ -32,9 +55,9 @@ $ ipcrm --all=sem
 *   若 semflags/shmflags 不含有 `IPC_EXCL` , 则函数会返回已创建 IPC 信号量的 id
 *   可以通过对于同一 key, 改变 flags 的方法, 灵活操控 semget/shmget 函数, 以达到创建/获取特定 IPC 信号量的目的, 使得进程间能够进行信息交流
 
-## BUGs
+### BUGs
 
-### semctl
+#### semctl
 
 The **calling** program must define this union as follows:
 
@@ -48,26 +71,26 @@ union semun {
 };
 ```
 
-### semget/shmget
+#### semget/shmget
 
 *   `sem_flags` 含有 `IPC_EXCL` 时, 不会返回已经存在的 IPC 信号量
 *   当 key 为 0 时, 返回 id 变成随机值(不再固定)
 
-### copy break condition
+#### copy break condition
 
 当从 S buffer 接收到 EOF 后, 不应该立即 break, 应将 EOF 送入 T buffer 后, 再进行 break, 否则会产生死锁
 
-### EOF
+#### EOF
 
 EOF 不能写入 dist file, 否则会造成乱码
 
-### Dead Lock
+#### Dead Lock
 
 *   problem: get 结束后, 会 detach bufs_map, 导致 copy 得不到正确的信号量(bufs_full)而阻塞, 产生死锁
 *   solution: 由于 get 与 copy 间存在同步关系, get 必定先于 copy 结束, 故不应在 get 中 detach bufs_map. 同理, 在 copy 不应 detach buft_map.
 *   solution: 为了使得 get 程序不太快于 copy 程序结束, copy 程序不太快于 put 程序结束, 使其都在退出前休眠 500 ms.
 
-## Debug
+### Debug
 
 ```sh
 # debug parent process only
---
+++ lab2-1/src/main.c
@@ -0,0 +1,75 @@
+/*!
+ * \file main.c
+ * \brief entry file for pthread lab
+ *
+ * \author sabertazimi, <sabertazimi@gmail.com>
+ * \version 1.0
+ * \date 2016-11-11
+ * \license MIT
+ */
+
+#undef DEBUG
+#define DEBUG   ///< macro for enable/disable debug functions
+// #undef DEBUG
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include "utils/utils.h"
+#include "semaphore/semaphore.h"
+
+static const int num_windows = 10;  ///< number of windows selling tickets
+static int num_tickets = 100;       ///< number of available tickets
+static semaphore_t mutex;           ///< mutex for adding/subing num_tickets
+
+void window(void *args) {
+    int id = *(int *)args;
+
+    srand((unsigned)time(NULL));
+
+    while (1) {
+        sleep(rand() % 3);
+
+        mutex->P(mutex);
+
+        if (num_tickets <= 0) {
+            mutex->V(mutex);
+            break;
+        } else {
+            num_tickets--;
+            LOG("window %2d sell out 1 ticket, now available %3d\n", id, num_tickets);
+            mutex->V(mutex);
+        }
+    }
+}
+
+int main(void) {
+    int ret;                                ///< return value of pthread_create function
+    int windows_id[num_windows];            ///< No. id for every single window
+    pthread_t windows_pid[num_windows];     ///< thread id for every single window
+
+    srand((unsigned)time(NULL));
+
+    // create semaphore
+    mutex = semnew(1);
+    // LOG("Main: semid %d\n", mutex->semid);
+    // LOG("Main: semval %d\n", mutex->semval);
+
+    for (int i = 0; i < num_windows; i++) {
+        windows_id[i] = i + 1;
+        while ((ret = pthread_create(&windows_pid[i], NULL, (void *)window, &windows_id[i])) != 0);
+        // LOG("create No.%2d window with pid %d\n", windows_id[i], windows_pid[i]);
+    }
+
+    // wait for sub-thread finish
+    for (int i = 0; i < num_windows; i++) {
+        pthread_join(windows_pid[i], NULL);
+    }
+
+    // remove semaphore
+    mutex->del(mutex);
+
+    return 0;
+}
+
