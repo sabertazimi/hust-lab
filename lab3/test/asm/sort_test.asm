9ca7121e7d3197efab517e39ad2ffec1440317f2
sabertazimi <sabertazimi@gmail.com>
2016-12-15 10:58:23 +0800

    test(arch-lab3 cpu): get all tests passed
    issue sabertazimi/awesome-notes#99

--- lab3/test/asm/sort_test.asm
+++ lab3/test/asm/sort_test.asm
@@ -1,13 +1,8 @@
-#################################################################################
-#������ʵ��0-15���ֵ�Ԫ�Ľ�������,�˳������mars mips������������
-#����ʱ�뽫Mars Setting�е�Memory Configuration����ΪCompact��data at address 0
-#
-#################################################################################
  .text
 sort_init:
  addi $s0,$0,-1
  addi $s1,$0,0
- 
+
  sw $s0,0($s1)
  addi $s0,$s0,1
  addi $s1,$s1,4
@@ -56,26 +51,26 @@ sort_init:
     sw $s0,0($s1)
  addi $s0,$s0,1
  addi $s1,$s1,4
-   
+
  addi $s0,$s0,1
- 
- add $s0,$zero,$zero   
- addi $s1,$zero,60   #��������
+
+ add $s0,$zero,$zero        # init: lowest index
+ addi $s1,$zero,60          # init: highest index
+
 sort_loop:
- lw $s3,0($s0)     
- lw $s4,0($s1)
- slt $t0,$s3,$s4
- beq $t0,$0,sort_next   #��������
- sw $s3, 0($s1)
- sw $s4, 0($s0)   
+ lw $s3,0($s0)              # arr[low]
+ lw $s4,0($s1)              # arr[high]
+ slt $t0,$s3,$s4            # $t0 = arr[low] < arr[high]
+ beq $t0,$0,sort_next       # if $t0 = 1(arr[low] < arr[high])
+ sw $s3, 0($s1)             # swap(arr[low], arr[high])
+ sw $s4, 0($s0)             # swap(arr[low], arr[high])
 sort_next:
- addi $s1, $s1, -4   
- bne $s0, $s1, sort_loop  
+ addi $s1, $s1, -4          # high--
+ bne $s0, $s1, sort_loop    # if low != high, continue loop
 
- addi $s0,$s0,4
- addi $s1,$zero,60
- bne $s0, $s1, sort_loop
+ addi $s0,$s0,4             # low++(index <= low: sorted elements)
+ addi $s1,$zero,60          # reset high to highest
+ bne $s0, $s1, sort_loop    # all elements get sorted
 
- addi   $v0,$zero,10         # system call for exit
- # syscall                  # we are out of here.   
- #MIPS������ʵ��������ͣ��ָ��ʵ��syscall
+ addi   $v0,$zero,10        # system call for exit
+ syscall
