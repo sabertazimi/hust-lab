2fdf045a059dda6ae3a907955c1fd6db4af38a4e
sabertazimi <sabertazimi@gmail.com>
2017-01-20 22:37:31 +0800

    update(arch-pipeline-lib): implement data processing components
    *   bits adder
    *   bits shifter
    *   bits extender
    
    reference to harris.Digital Logic and Computer Architecture
    
    issue #1 ideal pipeline MIPS processor
    issue sabertazimi/awesome-notes#99

---
+++ pipeline/verilog/shift2.v
@@ -0,0 +1,17 @@
+/**
+ * @module shift2
+ * @brief 2 bits left shifter for PC update logic
+ * @param DATA_WIDTH data width
+ * @input din raw data
+ * @ouput dout shifted data
+ */
+module shift2
+#(paramter DATA_WIDTH = 32)
+(
+    input [DATA_WIDTH-1:0] din,
+    output [DATA_WIDTH-1:0] dout
+);
+
+    assign dout = din << 2;
+    
+endmodule // shift2
---
+++ pipeline/verilog/sigext.v
@@ -0,0 +1,18 @@
+/**
+ * @module sigext
+ * @brief signed bits extender
+ * @param INPUT_WIDTH input data width, extend it to DATA_WIDTH
+ * @param DATA_WIDTH data width
+ * @input din raw data
+ * @ouput dout extended data
+ */
+module sigext
+#(parameter INPUT_WIDTH = 16, DATA_WIDTH = 32)
+(
+    input [INPUT_WIDTH-1:0] din,
+    output [DATA_WIDTH-1:0] dout
+);
+
+    assign dout = {{(DATA_WIDTH-INPUT_WIDTH){din[INPUT_WIDTH-1]}}, din};
+    
+endmodule // sigext
