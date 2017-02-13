6bad03e6177834bbd9c17d8c7c5e3c961051dea9
sabertazimi <sabertazimi@gmail.com>
2017-02-13 15:56:45 +0800

    add(cs-arch-bgtz): implement bgtz instruction
    fix #7
    issue sabertazimi/awesome-notes#99

--- pipeline/test/extend.asm
+++ pipeline/test/extend.asm
@@ -1,4 +1,30 @@
+# spec for divu
 addi $s0, $zero, -1
 addi $s1, $zero, 2
 divu $s0, $s1
+
+# spec for mflo
 mflo $s1
+
+# spec for lb
+addi $s1, $zero, 0xff
+sw $s1, 0($zero)
+addi $s0, $zero, 0 	# reset
+lb $s0, 0($zero)
+addi $s1, $zero, 3
+sw $s1, 0($zero)
+addi $s0, $zero, 0	# reset
+lb $s0, 0($zero)
+
+# spec for bgtz
+addi $s0, $zero, 0	# reset
+addi $s1, $zero, -20
+bgtz $s1, not_jmp
+addi $s1, $zero, 20
+bgtz $s1, will_jmp
+not_jmp:
+addi $s0, $zero, 233
+will_jmp:
+addi $s0, $zero, 250
+addi $s1, $zero, 0
+bgtz $s1, not_jmp
