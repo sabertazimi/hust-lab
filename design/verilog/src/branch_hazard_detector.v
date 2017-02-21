79a9cd71342607260958e2b67db37098fef002e7
sabertazimi <sabertazimi@gmail.com>
2017-02-21 20:48:47 +0800

    add(arch-branch prediction-logisim): implement static branch prediction
    Reference:
    
    *   Interrupt: Dominic Sweetman. See MIPS Run (2nd Edition). San Francisco: Morgan Kaufmann, 2007
    *   Pipeline Basis 1: David Money Harris(美).数字设计和计算机体系结构（第二版）. 机械工业出版社, 2015
    *   Pipeline Basis 2: David Money Harris, Sarah Harris. Digital Design and Computer Architecture. San Francisco: Morgan Kaufmann, 2007
    *   Hazard Detection: John Hennessy, David Patterson. 计算机体系结构: 机械工业出版社, 2012
    
    issue sabertazimi/architecture#2
    issue sabertazimi/awesome-notes#99

--- design/verilog/src/branch_hazard_detector.v
+++ design/verilog/src/branch_hazard_detector.v
@@ -7,10 +7,9 @@
  * @input EX_RW RW value in EX stage
  * @input MEM_ramtoreg ramtoreg signal in EX stage
  * @input MEM_RW RW value in MEM stage
- * @input ID_jmp_need_reg signal imply whether current branch instructoin need read regs or not
- * @input ID_jmp_imm signal imply whether current instructoin is j/jal or not
- * @input ID_jmp_reg signal imply whether current instructoin is jr or not
- * @input ID_jmp_branch signal imply whether current instructoin is success branch or not
+ * @input ID_jmp_need_reg signal imply whether current branch instruction need read regs or not
+ * @input ID_jmp_reg signal imply whether current instruction is jr or not
+ * @input ID_misprediction signal imply whether current instruction is successful predict branch or not
  * @output flushD signal for flushing IF/ID pipeline register
  * @output flushE signal for flushing ID/EX pipeline register
  */
@@ -23,14 +22,13 @@ module branch_hazard_detector
     input MEM_ramtoreg,
     input [4:0] MEM_RW,
     input ID_jmp_need_reg,
-    input ID_jmp_imm,
     input ID_jmp_reg,
-    input ID_jmp_branch,
+    input ID_misprediction,
     output branch_flushD,
     output branch_flushE
 );
 
-    assign branch_flushD = ID_jmp_imm || ID_jmp_reg || ID_jmp_branch;
+    assign branch_flushD = ID_jmp_reg || ID_misprediction;
     assign branch_flushE = (ID_jmp_need_reg && EX_regwe && EX_RW != 0 && (EX_RW == ID_rs || EX_RW == ID_rt))
     || (ID_jmp_need_reg && MEM_ramtoreg && MEM_RW != 0 && (MEM_RW == ID_rs || MEM_RW == ID_rt));
         
