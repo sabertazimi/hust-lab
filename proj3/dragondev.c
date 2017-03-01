14f568ef340d72d2c6e234e8d47d78d9d98c0b23
sabertazimi <sabertazimi@gmail.com>
2017-03-01 11:38:16 +0800

    fix(os-final proj3-drivers): read/write function impl error
    mistakely write/read buf, should be buf+seek_pos.
    
    fix sabertazimi/hust-os-2017#9
    issue sabertazimi/hust-os-2017#4
    issue sabertazimi/awesome-notes#27

--- proj3/dragondev.c
+++ proj3/dragondev.c
@@ -1,41 +1,69 @@
 /*!
- * \file dragondevice.c
- * \brief
- *
- * \author sabertazimi, <sabertazimi@gmail.com>
- * \version 1.0
- * \date 2017
- * \license MIT
- */
+* \file dragondevice.c
+* \brief
+*
+* \author sabertazimi, <sabertazimi@gmail.com>
+* \version 1.0
+* \date 2017
+* \license MIT
+*/
 
 #include <linux/fs.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <asm/uaccess.h>
 
 // 处理版本问题CONFIG_MODVERSIONS
 #if CONFIG_MODVERSIONS == 1
-    #define MODVERSIONS
-    #include "linux/version.h"
+#define MODVERSIONS
+#include "linux/version.h"
 #endif
 
+#define BUF_SIZE 1000   ///< buffer size
+char buf[BUF_SIZE];     ///< device charaters buffer
+int dev_num = 0;        ///< device number
+int buf_size = 0;       ///< buffer current size containing characters
+int seek_pos = 0;       ///< current 
+
 static int dragondev_open(struct inode *inode, struct file *filp) {
+    seek_pos = 0;
     printk("Dragon device open success.\n");
     return 0;
 }
 
 static int dragondev_release (struct inode *inode, struct file *filp) {
-    printk("Dragon devicDragon devicease success.\n");
+    seek_pos = 0;
+    printk("Dragon device release success.\n");
     return 0;
 }
 
 static ssize_t dragondev_read (struct file *filp, char __user *target , size_t tsize, loff_t *offset) {
-    printk("Dragon device read success.\n");
-    return -1;
+    size_t read_size = buf_size - seek_pos;
+    read_size = read_size < tsize ? read_size : tsize;
+    
+    if (!copy_to_user((char *)target, buf+seek_pos, read_size)) {
+        seek_pos += read_size;
+        printk("Dragon device read success.\n");
+        return read_size;
+    } else {
+        printk("Dragon device read failed.\n");
+        return -1;
+    }
 }
 
 static ssize_t dragondev_write (struct file *filp,const char __user *target , size_t tsize, loff_t *offset) {
-    printk("Dragon device write success.\n");
-    return -1;
+    size_t write_size = BUF_SIZE - seek_pos;
+    write_size = write_size < tsize ? write_size : tsize;
+    
+    if(!copy_from_user((char *)buf+seek_pos, target, write_size)) {
+        seek_pos += write_size;
+        buf_size += write_size;
+        printk("Dragon device write success.\n");
+        return write_size;
+    } else {
+        printk("Dragon device write failed.\n");
+        return -1;
+    }
 }
 
 static const struct file_operations dragondev_fops = {
@@ -46,20 +74,19 @@ static const struct file_operations dragondev_fops = {
     .release = dragondev_release
 };
 
-int devnum = 0;
 
 int init_module(void) {
-    if ((devnum = register_chrdev(0, "dragondev", &dragondev_fops)) < 0) {
+    if ((dev_num = register_chrdev(0, "dragondev", &dragondev_fops)) < 0) {
         printk("Dragon device register failed.\n");
     } else {
         printk("Dragon device register success.\n");
     }
-
+    
     return 0;
 }
 
 void cleanup_module(void) {
-    unregister_chrdev(devnum, "dragondev");
+    unregister_chrdev(dev_num, "dragondev");
     printk("Dragon device unregister success.\n");
 }
 
--- proj3/dragondev_test.c
+++ proj3/dragondev_test.c
@@ -1,12 +1,12 @@
 /*!
- * \file dragondev_test.c
- * \brief
- *
- * \author sabertazimi, <sabertazimi@gmail.com>
- * \version 1.0
- * \date 2017
- * \license MIT
- */
+* \file dragondev_test.c
+* \brief
+*
+* \author sabertazimi, <sabertazimi@gmail.com>
+* \version 1.0
+* \date 2017
+* \license MIT
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -17,22 +17,34 @@
 
 int main(void) {
     int dragondev;
-    char buf[10];
-
+    char buf[1];
+    
     dragondev = open("/dev/dragondev", O_RDWR);
-
     if (dragondev == -1) {
         perror("Test failed.\n");
         exit(0);
     }
-
-    read(dragondev, buf, 10);
-
-    for (int i = 0; i < 10; i++) {
-        printf("%d\n", buf[i]);
+    
+    for (int i = 1; i <= 127; i++) {
+        buf[0] = i;
+        printf("write_size = %d\t", (int)write(dragondev, buf, 1));
     }
-
+    
     close(dragondev);
+    printf("\n\n");
+    
+    dragondev = open("/dev/dragondev", O_RDWR);
+    if (dragondev == -1) {
+        perror("Test failed.\n");
+        exit(0);
+    }
+    
+    for (int i = 1; i <= 127; i++) {
+        printf("read_size = %d, ", (int)read(dragondev, buf, 1));
+        printf("%c\n", buf[0]);
+    }
+    
+    close(dragondev);
+    
+    return 0;
 }
-
-
