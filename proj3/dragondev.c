2e9b591c1c1a45a3e58273c597654db9a9f3a912
sabertazimi <sabertazimi@gmail.com>
2017-03-01 10:03:13 +0800

    update(os-final proj3-drivers): add simple char device
    issue sabertazimi/hust-os-2017#4
    issue sabertazimi/awesome-notes#27

---
+++ proj3/dragondev.c
@@ -0,0 +1,70 @@
+/*!
+ * \file dragondevice.c
+ * \brief
+ *
+ * \author sabertazimi, <sabertazimi@gmail.com>
+ * \version 1.0
+ * \date 2017
+ * \license MIT
+ */
+
+#include <linux/fs.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+// 处理版本问题CONFIG_MODVERSIONS
+#if CONFIG_MODVERSIONS == 1
+    #define MODVERSIONS
+    #include "linux/version.h"
+#endif
+
+static int dragondev_open(struct inode *inode, struct file *filp) {
+    printk("Device open success.\n");
+    return 0;
+}
+
+static int dragondev_release (struct inode *inode, struct file *filp) {
+    printk("Device release success.\n");
+    return 0;
+}
+
+static ssize_t dragondev_read (struct file *filp, char __user *target , size_t tsize, loff_t *offset) {
+    printk("Device read success.\n");
+    return -1;
+}
+
+static ssize_t dragondev_write (struct file *filp,const char __user *target , size_t tsize, loff_t *offset) {
+    printk("Device write success.\n");
+    return -1;
+}
+
+static const struct file_operations dragondev_fops = {
+    .owner = THIS_MODULE,
+    .open = dragondev_open,
+    .read = dragondev_read,
+    .write = dragondev_write,
+    .release = dragondev_release
+};
+
+int devnum = 0;
+
+int init_module(void) {
+    if ((devnum = register_chrdev(0,"dragondev", &dragondev_fops))) {
+        printk("Device register failed.\n");
+    } else {
+        printk("Device register success.\n");
+    }
+
+    return 0;
+}
+
+void cleanup_module(void) {
+    unregister_chrdev(devnum, "dragondev");
+    printk("Device unregister success.\n");
+}
+
+MODULE_AUTHOR("sabertazimi");
+MODULE_DESCRIPTION("dragon device");
+MODULE_LICENSE("MIT");
+MODULE_VERSION("V1.0");
+
