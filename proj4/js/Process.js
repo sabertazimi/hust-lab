0f8c73e24b0199f61181f5108bc724ff54f35fee
sabertazimi <sabertazimi@gmail.com>
2017-03-03 10:36:08 +0800

    update(os-final proj4-process): encapsulate get process data function
    issue sabertazimi/hust-os-2017#5
    issue sabertazimi/awesome-notes#27

--- proj4/js/Process.js
+++ proj4/js/Process.js
@@ -12,25 +12,25 @@ const readProcessDir = (dir) => {
     let results = [];
     let list = '';
     let len;
-    
-    list = fs.readdirSync(dir, 'utf8'); 
-    
+
+    list = fs.readdirSync(dir, 'utf8');
+
     if (list.length === 0) {
         return null;
     }
-    
+
     list.forEach((file) => {
         let stat;
         file = path.resolve(dir, file);
-        
+
         stat = fs.statSync(file);
-        
+
         // find a process info directory, push it into results
         if (stat && stat.isDirectory && /^[0-9]*$/.test(file.split('/')[2])) {
             results = results.concat(file);
         }
     });
-    
+
     return results;
 };
 
@@ -38,13 +38,13 @@ const getTotalCPU = () => {
     let cpuData = fs.readFileSync('/proc/stat', 'utf8');
     let cpuStat = String.prototype.split.call(cpuData, '\n');
     let cpuUsage = String.prototype.split.call(cpuStat[0], /\s+/);
-    
+
     let total = 0;
-    
+
     for (let i = 1; i < 10; i++) {
         total += parseInt(cpuUsage[i], 10);
     }
-    
+
     return total;
 };
 
@@ -56,17 +56,22 @@ const readProcessStats = () => {
         const file = path.resolve(dir, 'stat');
         const data = fs.readFileSync(file, 'utf8');
         const stat = String.prototype.split.call(data, /\s+/);
-        
+
         // important file: /proc/*/statm => memory info of process
         const filem = path.resolve(dir, 'statm');
         const datam = fs.readFileSync(filem, 'utf8');
         const statm = String.prototype.split.call(datam, /\s+/);
-        
-        const rawData = [stat, statm];
-        
+
+        // important file: /proc/*/statm => memory info of process
+        const files = path.resolve(dir, 'status');
+        const datas = fs.readFileSync(files, 'utf8');
+        const status = String.prototype.split.call(datas, '\n');
+
+        const rawData = [stat, statm, status];
+
         processRawData.set(stat[pid], rawData);
     });
-    
+
     return processRawData;
 };
 
@@ -85,8 +90,12 @@ const sleepTime = 1000;
 const pagesize = 4096;  // getconf PAGE_SIZE
 const memUse = 1;
 
+// enum for status
+const VmRSS = 21;
+
 const stat = 0;
 const statm = 1;
+const status = 2;
 
 const P_TOTAL = 0;
 const P_CPU = 1;
@@ -94,59 +103,73 @@ const P_CPUUsage = 2;
 const P_PID = 3;
 const P_NAME = 4;
 
+const getProcessData = (key, rawData) => {
+    const totalCPU = getTotalCPU();
+    const processCPU = parseInt(rawData[stat][utime]) + parseInt(rawData[stat][stime]);
+    const processName = rawData[stat][comm].replace('(', '').replace(')', '');
+
+    let processMem = String.prototype.split.call(rawData[status][VmRSS], /\s+/);
+    if (processMem[0] === 'VmRSS:') {
+        processMem = parseInt(processMem[1]);
+    } else {
+        processMem = 0;
+    }
+
+    const processData = [totalCPU, processCPU, 0, key, processName, processMem];
+
+    return processData;
+}
+
 const getProcessItems = () => {
     let processItems = new Map();
     let processRawData = readProcessStats();
-    
+
     for (let key of processRawData.keys()) {
         const rawData = processRawData.get(key);
-        const totalCPU = getTotalCPU();
-        const processCPU = parseInt(rawData[stat][utime]) + parseInt(rawData[stat][stime]);
-        const processName = rawData[stat][comm].replace('(', '').replace(')', '');
-        const processData = [totalCPU, processCPU, 0, key, processName];
-        processItems.set(key, processData);
+        const [totalCPU, processCPU, cpuUsage, proceesID, processName, processMem] = getProcessData(key, rawData);
+        const processItem = [totalCPU, processCPU, cpuUsage, processName, processName, processMem];
+        processItems.set(key, processItem);
     }
-    
+
     const date = new Date();
     let curDate = null;
     do {
         curDate = new Date();
-    } while (curDate - date < sleepTime);    
-    
+    } while (curDate - date < sleepTime);
+
     processRawData = readProcessStats();
-    
+
     for (let key of processRawData.keys()) {
         if (processItems.has(key)) {
             // update information
-            // calculate cpu usage
             const rawData = processRawData.get(key);
-            const totalCPU2 = getTotalCPU();
-            const processCPU2 = parseInt(rawData[stat][utime]) + parseInt(rawData[stat][stime]);
-            const processData = processItems.get(key);
-            const totalCPU1 = processData[P_TOTAL];
-            const processCPU1 = processData[P_CPU];
-            const cpuUsage = 100 * (processCPU2 * 1.0 - processCPU1 * 1.0) / (totalCPU2 * 1.0 - totalCPU1 * 1.0);
-            const processName = rawData[stat][comm].replace('(', '').replace(')', '');
-            const newProcessData = [totalCPU2, processCPU2, cpuUsage, key, processName];
-            processItems.set(key, newProcessData);
+            let [totalCPU2, processCPU2, cpuUsage, processID, processName, processMem] = getProcessData(key, rawData);
+
+            // calculate cpu usage
+            // update cpu usage
+            const processItem = processItems.get(key);
+            const totalCPU1 = processItem[P_TOTAL];
+            const processCPU1 = processItem[P_CPU];
+            cpuUsage = 100 * (processCPU2 * 1.0 - processCPU1 * 1.0) / (totalCPU2 * 1.0 - totalCPU1 * 1.0);
+
+            const newProcessItem = [totalCPU2, processCPU2, cpuUsage, processID, processName, processMem];
+            processItems.set(key, newProcessItem);
         } else {
             // add new process
             const rawData = processRawData.get(key);
-            const totalCPU = getTotalCPU();
-            const processCPU = parseInt(rawData[stat][utime]) + parseInt(rawData[stat][stime]);
-            const processName = rawData[stat][comm].replace('(', '').replace(')', '');
-            const processData = [totalCPU, processCPU, 0, key, processName];
-            processItems.set(key, processData);
+            const [totalCPU, processCPU, cpuUsage, proceesID, processName, processMem] = getProcessData(key, rawData);
+            const processItem = [totalCPU, processCPU, cpuUsage, processID, processName, processMem];
+            processItems.set(key, processItem);
         }
     }
-    
+
     // remove missing process
     for (let key of processItems.keys()) {
         if (!processRawData.has(key)) {
             processItems.delete(key);
         }
     }
-    
+
     //   console.log(`${stat[comm]} pid: ${stat[pid]}`);
     //   console.log(`${stat[comm]} ttyid: ${stat[tty]}`);
     //   console.log(`${stat[comm]} priority: ${stat[priority]}`);
@@ -155,17 +178,17 @@ const getProcessItems = () => {
     //   console.log(`${stat[comm]} virtual memory size: ${stat[vsize]}`);
     //   console.log(`${stat[comm]} resident set size: ${stat[rss]}`);
     //   console.log(`memory usage : ${statm[memUse]}`);
-    
+
     return processItems;
 };
 
 module.exports = getProcessItems;
 
-// const processItems = getProcessItems();
+const processItems = getProcessItems();
 // for (let value of processItems.values()) {
 //     console.log(`${value[P_PID]} => ${value[P_CPUUsage]}`);
 // }
-// 
+//
 // console.log(processItems.size);
 
 // console.log(processItems);
