6c75ff51f71ffb6ba2a3f38e645c3d748cb5a5e9
sabertazimi <sabertazimi@gmail.com>
2017-03-04 15:22:30 +0800

    update(os-final proj5-main): add repl
    issue sabertazimi/hust-os-2017#6
    issue sabertazimi/awesome-notes#27

---
+++ proj5/Repl.js
@@ -0,0 +1,11 @@
+/*
+ * Repl.js
+ * Copyright (C) 2017 sabertazimi <sabertazimi@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+class Repl {
+}
+
+module.exports = Repl;
---
+++ proj5/main.js
@@ -0,0 +1,14 @@
+/*
+ * main.js
+ * Copyright (C) 2017 sabertazimi <sabertazimi@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+'use strict';
+
+const Imfs = require('./Imfs.js');
+
+let imfs = new Imfs();
+
+console.log(imfs);
