71c203264d3270755eee09ed2fee47b02ca66be4
sabertazimi <sabertazimi@gmail.com>
2017-03-14 13:57:01 +0800

    add(network-email): set up electron
    *   window size
    *   builder/packager
    
    issue sabertazimi/awesome-notes#99

---
+++ email/src/js/view.js
@@ -0,0 +1,12 @@
+/*
+ * view.js
+ * Copyright (C) 2017 sabertazimi <sabertazimi@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+(function(){
+  'use strict';
+
+
+})();
---
+++ email/test/main.js
@@ -0,0 +1,17 @@
+/*
+ * main.js
+ * Copyright (C) 2017 sabertazimi <sabertazimi@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+const should = require('should');
+
+describe('', () => {
+    describe("", () => {
+        it("should x", () => {
+            // should.be.eql()
+        });
+    });
+});
+
