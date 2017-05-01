32342f2aaf5012c9b3607930d365871747219f9f
sabertazimi <sabertazimi@gmail.com>
2017-05-01 21:47:14 +0800

    add(arch-quantization): add date for 63/127/1023 step
    issue sabertazimi/awesome-notes#99

--- quantization/lab2/t2.c
+++ quantization/lab2/t2.c
@@ -8,10 +8,16 @@ int main(void ) {
     double timeuse;
     int i, j;
     int sizes[18];
+    FILE *fp;
 
     for(i = 0; i < 18; i ++)
     {
         sizes[i] = (1 << i) << 10;                  //计算出1k, 2k, 4k, ... , 128M的数值大小
+    }
+
+    if ((fp = fopen("./63.dat", "w+")) == NULL) {
+        fprintf(stderr, "Create data file failed.\n");
+        return -1;
     }
 
     for(i = 0; i < 18; i ++)
@@ -26,16 +32,18 @@ int main(void ) {
 
         for(j = 0; j < (1 << 25); j++)                  //访问2^25次
         {
-            result += data[((long long)1023* (j % size)) % size];                  //以127为步长访问
+            result += data[((long long)63* (j % size)) % size];                  //以127为步长访问
         }
 
         gettimeofday(&t2, NULL);                    //计时结束
         timeuse = t2.tv_sec - t1.tv_sec
             + (t2.tv_usec - t1.tv_usec)/1000000.0;      //计算时长
 
-        printf("%dK, %fsecs, result:%d\n",
-        size / 1024, timeuse, result);              //打印结果
+        printf("%dK, %fsecs, result:%d\n", size / 1024, timeuse, result);              //打印结果
+        fprintf(fp, "%d %f\n", size / 1024, timeuse);                       // 保存结果
     }
+
+    fclose(fp);
 
     return 0;
 }
