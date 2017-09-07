ed4b3b72e204049fdac8a3d39095211c361c5101
tracer-ics2017 <tracer@njuics.org>
2017-09-07 22:14:04 +0800

    >  compile U201414800 sabertazimi Linux 17aa0d73d5e3 4.8.0-46-generic #49~16.04.1-Ubuntu SMP Fri Mar 31 14:51:03 UTC 2017 x86_64 GNU/Linux  22:14:04 up 3 days, 14:04,  3 users,  load average: 0.39, 0.39, 0.36 1657d26d8fa1c8f62a98a0af1b57365a1f2e92a

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -149,6 +149,7 @@ static bool make_token(char *e) {
           case TK_REG:
             tokens[nr_token].type = rules[i].token_type;
             strncpy(tokens[nr_token].str, substr_start, substr_len);
+            tokens[nr_token].str[substr_len] = '\0';
             ++nr_token;
             break;
           default:
