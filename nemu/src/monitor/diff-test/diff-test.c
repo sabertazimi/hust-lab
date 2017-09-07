6d84b8864b8988174d9542ad4b1c12d54bce64dd
tracer-ics2017 <tracer@njuics.org>
2017-09-08 00:08:37 +0800

    >  compile U201414800 sabertazimi Linux 17aa0d73d5e3 4.8.0-46-generic #49~16.04.1-Ubuntu SMP Fri Mar 31 14:51:03 UTC 2017 x86_64 GNU/Linux  00:08:37 up 3 days, 15:59,  3 users,  load average: 0.05, 0.29, 0.32 3284cd85db98f490a620fa513fa807668200f117

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -156,17 +156,17 @@ void difftest_step(uint32_t eip) {
     || r.esi != cpu.esi || r.edi != cpu.edi
     || r.eip != cpu.eip) {
     diff = true;
-    Warn("    \tNemu\t\t\t\tQemu");
-    Warn("$eax\t%d(0x%08x)\t\t%d(0x%08x)", cpu.eax, cpu.eax, r.eax, r.eax);
-    Warn("$ecx\t%d(0x%08x)\t\t%d(0x%08x)", cpu.ecx, cpu.ecx, r.ecx, r.ecx);
-    Warn("$edx\t%d(0x%08x)\t\t%d(0x%08x)", cpu.edx, cpu.edx, r.edx, r.edx);
-    Warn("$ebx\t%d(0x%08x)\t\t%d(0x%08x)", cpu.ebx, cpu.ebx, r.ebx, r.ebx);
-    Warn("$esp\t%d(0x%08x)\t\t%d(0x%08x)", cpu.esp, cpu.esp, r.esp, r.esp);
-    Warn("$ebp\t%d(0x%08x)\t\t%d(0x%08x)", cpu.ebp, cpu.ebp, r.ebp, r.ebp);
-    Warn("$esi\t%d(0x%08x)\t\t%d(0x%08x)", cpu.esi, cpu.esi, r.esi, r.esi);
-    Warn("$edi\t%d(0x%08x)\t\t%d(0x%08x)", cpu.edi, cpu.edi, r.edi, r.edi);
-    Warn("$eip\t%d(0x%08x)\t\t%d(0x%08x)", cpu.eip, cpu.eip, r.eip, r.eip);
-    Warn("$eflags\t%d(0x%08x)\t\t%d(0x%08x)", cpu.eflags.val, cpu.eflags.val, r.eflags, r.eflags);
+    Warn("    \tNemu\t\t\t\t\tQemu");
+    Warn("$eax\t%d(0x%08x)\t\t\t%d(0x%08x)", cpu.eax, cpu.eax, r.eax, r.eax);
+    Warn("$ecx\t%d(0x%08x)\t\t\t%d(0x%08x)", cpu.ecx, cpu.ecx, r.ecx, r.ecx);
+    Warn("$edx\t%d(0x%08x)\t\t\t%d(0x%08x)", cpu.edx, cpu.edx, r.edx, r.edx);
+    Warn("$ebx\t%d(0x%08x)\t\t\t%d(0x%08x)", cpu.ebx, cpu.ebx, r.ebx, r.ebx);
+    Warn("$esp\t%d(0x%08x)\t\t\t%d(0x%08x)", cpu.esp, cpu.esp, r.esp, r.esp);
+    Warn("$ebp\t%d(0x%08x)\t\t\t%d(0x%08x)", cpu.ebp, cpu.ebp, r.ebp, r.ebp);
+    Warn("$esi\t%d(0x%08x)\t\t\t%d(0x%08x)", cpu.esi, cpu.esi, r.esi, r.esi);
+    Warn("$edi\t%d(0x%08x)\t\t\t%d(0x%08x)", cpu.edi, cpu.edi, r.edi, r.edi);
+    Warn("$eip\t%d(0x%08x)\t\t\t%d(0x%08x)", cpu.eip, cpu.eip, r.eip, r.eip);
+    Warn("$eflags\t%d(0x%08x)\t\t\t%d(0x%08x)", cpu.eflags.val, cpu.eflags.val, r.eflags, r.eflags);
   }
 
   if (diff) {
