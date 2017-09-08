37f66eca68c25bd20a2e0d0a818d61c425c7778d
tracer-ics2017 <tracer@njuics.org>
2017-09-08 10:03:26 +0000

    >  compile U201414800 sabertazimi Linux 17aa0d73d5e3 4.8.0-46-generic #49~16.04.1-Ubuntu SMP Fri Mar 31 14:51:03 UTC 2017 x86_64 GNU/Linux  10:03:26 up 4 days,  9:53,  3 users,  load average: 0.13, 0.26, 0.30 6fc97fec9ab767cc932ca31610f441d0246bec8b

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -63,6 +63,7 @@ make_EHelper(cwtl) {
 make_EHelper(movsx) {
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
   rtl_sext(&t2, &id_src->val, id_src->width);
+  Log("id_dest %d, id_src %d", id_dest->width, id_src->width);
   operand_write(id_dest, &t2);
   print_asm_template2(movsx);
 }
