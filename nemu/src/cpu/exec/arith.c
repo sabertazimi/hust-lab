7bb738a874814e18315e431eca4d4f0bb7ed79e0
tracer-ics2017 <tracer@njuics.org>
2017-09-08 03:19:51 +0000

    >  compile U201414800 sabertazimi Linux 17aa0d73d5e3 4.8.0-46-generic #49~16.04.1-Ubuntu SMP Fri Mar 31 14:51:03 UTC 2017 x86_64 GNU/Linux  03:19:51 up 4 days,  3:10,  3 users,  load average: 0.21, 0.26, 0.30 4efe9e6a7172f81f601c054ed85b5a588e81135

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -61,7 +61,20 @@ make_EHelper(inc) {
 }
 
 make_EHelper(dec) {
-  TODO();
+  rtl_li(&t3, 1);
+  rtl_sub(&t2, &id_dest->val, &t3);
+  operand_write(id_dest, &t2);
+
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  rtl_sltu(&t0, &id_dest->val, &t2);
+  rtl_set_CF(&t0);
+
+  rtl_xor(&t0, &id_dest->val, &t3);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
 
   print_asm_template1(dec);
 }
