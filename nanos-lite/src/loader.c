1d81549668450d046bd42ba0a82eadd1109c197a
U201414800-Liu Yilong <sabertazimi@gmail.com>
2017-09-10 09:53:39 +0000

    update(ics2017-pa3.1): implement 'loader' function
    issue sabertazimi/ics-2017#3

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,15 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+extern size_t get_ramdisk_size();
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  size_t ramdisk_size = get_ramdisk_size();
+  // char* buf = kalloc((ramdisk_size + 3) * sizeof(char));
+  char buf[30000];
+  ramdisk_read(buf, 0, ramdisk_size);
+  memcpy(DEFAULT_ENTRY, buf, ramdisk_size);
+  // kfree(buf);
   return (uintptr_t)DEFAULT_ENTRY;
 }
