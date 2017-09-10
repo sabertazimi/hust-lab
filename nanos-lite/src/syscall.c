55900c95d3bd3aba357e8f47022b89d8ed40b6ee
U201414800-Liu Yilong <sabertazimi@gmail.com>
2017-09-10 16:08:54 +0000

    update(ics2017-pa3.1): implement 'SYSCALL_ARGx' and exit syscall
    issue sabertazimi/ics-2017#3

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,14 +6,20 @@ _RegSet* do_syscall_none(_RegSet *r) {
   return r;
 }
 
+_RegSet* do_syscall_exit(_RegSet *r) {
+  _halt(SYSCALL_ARG2(r));
+  return r;
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
-  Log("syscall");
 
   switch (a[0]) {
     case SYS_none:
       return do_syscall_none(r);
+    case SYS_exit:
+      return do_syscall_exit(r);
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
