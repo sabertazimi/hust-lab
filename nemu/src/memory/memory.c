9a4ee7aa95894c08224d0eaefbe21b5493723e3e
tracer-ics2017 <tracer@njuics.org>
2017-09-12 01:41:30 +0000

    >  compile U201414800 sabertazimi Linux 17aa0d73d5e3 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 GNU/Linux  01:41:30 up 16:53,  3 users,  load average: 0.88, 0.57, 0.43 3cef8069b3c73b2b14a7733155a0efbd212b1419

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -64,8 +64,7 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 uint32_t vaddr_read(vaddr_t addr, int len) {
   if (PDX(addr) != PDX(addr + len) || PTX(addr) != PTX(addr + len)) {
     // cross the page boundary
-    // Assert(0, "cross the page boundary when read memory");
-    return paddr_read(addr, len);
+    Assert(0, "cross the page boundary when read 0x%08x memory", addr);
   } else {
     return paddr_read(addr, len);
   }
@@ -74,8 +73,7 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
   if (PDX(addr) != PDX(addr + len) || PTX(addr) != PTX(addr + len)) {
     // cross the page boundary
-    // Assert(0, "cross the page boundary when write memory");
-    paddr_write(addr, len, data);
+    Assert(0, "cross the page boundary when write 0x%08x memory", addr);
   } else {
     paddr_write(addr, len, data);
   }
