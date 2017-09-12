ca6e34a0b425784f786592257d187685452bd15a
tracer-ics2017 <tracer@njuics.org>
2017-09-12 07:08:50 +0000

    >  compile U201414800 sabertazimi Linux 17aa0d73d5e3 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 GNU/Linux  07:08:50 up 22:21,  3 users,  load average: 0.25, 0.21, 0.26 f190873d471e604b22b9a9dbcb3cf169655ea049

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -9,7 +9,8 @@ void init_ramdisk(void);
 void init_device(void);
 void init_irq(void);
 void init_fs(void);
-uint32_t loader(_Protect *, const char *);
+
+extern void load_prog(const char *filename);
 
 int main() {
 #ifdef HAS_PTE
@@ -30,8 +31,7 @@ int main() {
 
   init_fs();
 
-  uint32_t entry = loader(NULL, "/bin/pal");
-  ((void (*)(void))entry)();
+  load_prog("/bin/dummy");
 
   panic("Should not reach here");
 }
